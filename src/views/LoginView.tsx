6a9ccac4d4e224b7629fddf5207bbfced233e694
Lee Suho <riemannulus@hitagi.moe>
2020-06-09 08:45:19 +0900

    로그인 기능 구현

--- src/views/LoginView.tsx
+++ src/views/LoginView.tsx
@@ -13,6 +13,17 @@ const QUERY_CRYPTKEY = gql`
     }
 `
 
+const GET_DECRYPTKEY = gql`
+  query decreyptedPrivateKey($address: Address, $passphrase: String){
+      keyStore {
+            decryptedPrivateKey(
+                address: $address,
+                passphrase: $passphrase,
+            )
+        }
+    }
+`;
+
 export default function LoginView(props: any) {
     const { loading, error, data } = useQuery(QUERY_CRYPTKEY)
     error != undefined ? console.log(error) : null
@@ -26,7 +37,7 @@ export default function LoginView(props: any) {
                     </div>
                     {
                         loading || error != undefined ?
-                            <WaitComponent 
+                            <WaitComponent
                                 error={error}
                                 loading={loading}
                             /> :
@@ -62,15 +73,19 @@ function WaitComponent(props: any) {
 }
 
 function LoginComponent(props: any) {
+    //TODO: 플레이스홀더로 대체하고, 밑에서 input이 아니라 select로 바꿔서 여러 키 다 보여줄 수 있게 변경
     const [address, setAddress] = useState(props.keyStore.protectedPrivateKeys[0].address);
     const [passphrase, setPassphrase] = useState('placeholder');
-    const [privateKey, setPrivateKey] = useState('username22');
-
-    console.log(props.error)
+    const { loading, error, data } = useQuery(GET_DECRYPTKEY, {
+        variables: {
+            address: address,
+            passphrase: passphrase
+        },
+    })
 
     const handleAccount = () => {
         props.setAddress(address);
-        props.setPrivateKey(privateKey);
+        props.setPrivateKey(data.keyStore.decryptPrivateKey);
         props.setLogin(true);
     }
 
@@ -79,9 +94,9 @@ function LoginComponent(props: any) {
             <form>
                 <label>Address</label> <input value={address} onChange={event => { setAddress(event.target.value); console.log(address) }} type="text"></input>
                 <br />
-                <label>Passphrase</label> <input type="password" onChange={event => { setPassphrase(event.target.value) }}></input>
+                <label>Passphrase</label> <input type="password" onChange={event => { setPassphrase(event.target.value); }}></input>
             </form>
-            <button onClick={event => { handleAccount() }}>Login </button>
+            <button disabled={error != undefined} onClick={event => { handleAccount() }}>Login </button>
         </div>
     )
 }
