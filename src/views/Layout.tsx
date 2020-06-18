26060ee002fdcc2d911e5b80ed0f5588ab17a98b
Lee Suho <riemannulus@hitagi.moe>
2020-06-18 20:43:38 +0900

    쓸모없는 코드 정리

--- src/views/Layout.tsx
+++ src/views/Layout.tsx
@@ -1,18 +1,9 @@
 import * as React from 'react';
-import { Link } from 'react-router-dom';
 
 export interface ILayoutProps {
 }
 
 export const Layout: React.FunctionComponent<ILayoutProps> = ({ children }) => {
-
-    const getTime = () => {
-        let now = new Date();
-        let year = now.getFullYear().toString();
-        let month = now.getMonth().toString().padStart(2, "0");
-        let day = now.getDate().toString().padStart(2, "0");
-        return [year, month, day].reduce((prev, curr) => prev + "-" + curr);
-    }
 
     return (
         <div className="layout">
