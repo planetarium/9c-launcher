a11c511028af5c7f9f9b53afc8393f403c8a9109
Lee Suho <riemannulus@hitagi.moe>
2020-06-25 17:18:41 +0900

    스냅샷 다운로드 시 버튼 비활성화

--- src/components/DownloadSnapshotButton.tsx
+++ src/components/DownloadSnapshotButton.tsx
@@ -1,47 +1,59 @@
-import * as React from 'react';
+import * as React from "react";
 
-import { ipcRenderer, IpcRendererEvent } from 'electron';
-import { Button } from '@material-ui/core';
+import { ipcRenderer, IpcRendererEvent } from "electron";
+import { Button } from "@material-ui/core";
 
 interface IDownloadSnaphostProps {
-    setExtractState: React.Dispatch<React.SetStateAction<boolean>>
-    setDownloadState: React.Dispatch<React.SetStateAction<boolean>>
-    progress: number,
-    setProgress: React.Dispatch<React.SetStateAction<number>> 
+  disabled: boolean;
+  setExtractState: React.Dispatch<React.SetStateAction<boolean>>;
+  setDownloadState: React.Dispatch<React.SetStateAction<boolean>>;
+  progress: number;
+  setProgress: React.Dispatch<React.SetStateAction<number>>;
 }
 
 const DownloadSnapshotButton = (props: IDownloadSnaphostProps) => {
+  ipcRenderer.on("extract progress", (event, progress) => {
+    props.setExtractState(true);
+    props.setProgress(progress * 100);
+  });
 
-    ipcRenderer.on("extract progress", (event, progress) => {
-        props.setExtractState(true);
-        props.setProgress(progress * 100);
-    })
+  ipcRenderer.on("extract complete", (event) => {
+    props.setExtractState(false);
+  });
 
-    ipcRenderer.on("extract complete", (event) => {
-        props.setExtractState(false);
-    })
+  ipcRenderer.on(
+    "download progress",
+    (event: IpcRendererEvent, progress: IDownloadProgress) => {
+      props.setDownloadState(true);
+      props.setProgress(progress.percent * 100);
+    }
+  );
 
-    ipcRenderer.on("download progress", (event: IpcRendererEvent, progress: IDownloadProgress) => {
-        props.setDownloadState(true);
-        props.setProgress(progress.percent * 100);
-    });
+  ipcRenderer.on(
+    "download complete",
+    (event: IpcRendererEvent, path: string) => {
+      props.setDownloadState(false);
+    }
+  );
 
-    ipcRenderer.on("download complete", (event: IpcRendererEvent, path: string) => {
-        props.setDownloadState(false);
-    });
-
-    const downloadSnapShot = () => {
-        const options: IDownloadOptions = {
-            properties: {}
-        }
-        ipcRenderer.send("download snapshot", options);
+  const downloadSnapShot = () => {
+    const options: IDownloadOptions = {
+      properties: {}
     };
-    return <Button
-            onClick={(event: React.MouseEvent) => { downloadSnapShot() }}
-            variant="contained"
-            color="primary">
-                Download Snapshot
-           </Button>
-}
+    ipcRenderer.send("download snapshot", options);
+  };
+  return (
+    <Button
+      disabled={props.disabled}
+      onClick={(event: React.MouseEvent) => {
+        downloadSnapShot();
+      }}
+      variant="contained"
+      color="primary"
+    >
+      Download Snapshot
+    </Button>
+  );
+};
 
 export default DownloadSnapshotButton;
