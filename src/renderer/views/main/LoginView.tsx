7ed954f8fcbfd06fb936938c68ba49c21160c090
Lee Suho <riemannulus@hitagi.moe>
2020-06-27 15:33:19 +0900

    로그인 두 번 눌러야 하는 문제 수정

--- src/renderer/views/main/LoginView.tsx
+++ src/renderer/views/main/LoginView.tsx
@@ -91,23 +91,8 @@ const LoginComponent = observer((props: ILoginComponentProps) => {
       : accountStore.addAddress(value);
   });
 
-  const getPrivateKey = (): string | undefined => {
-    getDecreyptedKey({
-      variables: {
-        address: accountStore.selectAddress,
-        passphrase: passphrase,
-      },
-    });
-    return data?.keyStore?.decryptedPrivateKey;
-  };
-
-  const handleAccount = () => {
-    const privateKey = getPrivateKey();
-    if (privateKey === undefined) {
-      setSnackbarStatus(true);
-      return;
-    }
-
+  if (data?.keyStore?.decryptedPrivateKey !== undefined) {
+    const privateKey = data.keyStore.decryptedPrivateKey;
     accountStore.setPrivateKey(privateKey);
     accountStore.toggleLogin();
 
@@ -134,6 +119,15 @@ const LoginComponent = observer((props: ILoginComponentProps) => {
       .then((body) => console.log(body))
       .then((_) => {})
       .catch((error) => console.log(error));
+  }
+
+  const handleAccount = () => {
+    getDecreyptedKey({
+      variables: {
+        address: accountStore.selectAddress,
+        passphrase: passphrase,
+      },
+    });
   };
 
   const handleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
