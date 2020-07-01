078ff565a472c64d46b040b7b48deca5fd992916
Lee Suho <riemannulus@hitagi.moe>
2020-07-01 11:14:08 +0900

    현재 마이닝 옵션 상태별 라우팅 페이지 다르게 보여줌

--- src/renderer/views/login/LoginView.tsx
+++ src/renderer/views/login/LoginView.tsx
@@ -9,100 +9,114 @@ import Snackbar from "@material-ui/core/Snackbar";
 import { AccountSelect } from "../../components/AccountSelect";
 import ClearCacheButton from "../../components/ClearCacheButton";
 
-const LoginView = observer((props: IStoreContainer) => {
-  const [passphrase, setPassphrase] = useState("");
-  const [openSnackbar, setSnackbarStatus] = useState(false);
-  const { accountStore, routerStore } = props;
-  const [
-    getDecreyptedKey,
-    { loading, data },
-  ] = useDecreyptedPrivateKeyLazyQuery();
+const LoginView = observer(
+  ({ accountStore, routerStore, standaloneStore }: IStoreContainer) => {
+    const [passphrase, setPassphrase] = useState("");
+    const [openSnackbar, setSnackbarStatus] = useState(false);
+    const [
+      getDecreyptedKey,
+      { loading, data },
+    ] = useDecreyptedPrivateKeyLazyQuery();
 
-  React.useEffect(() => {
-    if (data?.keyStore?.decryptedPrivateKey !== undefined) {
-      const privateKey = data.keyStore.decryptedPrivateKey;
-      accountStore.setPrivateKey(privateKey);
-      accountStore.toggleLogin();
-      routerStore.push("/lobby/mining");
-    }
-  });
-
-  const handleSubmit = () => {
-    getDecreyptedKey({
-      variables: {
-        address: accountStore.selectedAddress,
-        passphrase: passphrase,
-      },
+    React.useEffect(() => {
+      if (data?.keyStore?.decryptedPrivateKey !== undefined) {
+        const privateKey = data.keyStore.decryptedPrivateKey;
+        accountStore.setPrivateKey(privateKey);
+        accountStore.toggleLogin();
+        if (standaloneStore.NoMiner) {
+          routerStore.push("/lobby/mining");
+        } else {
+          routerStore.push("/lobby");
+          try {
+            standaloneStore.initStandalone(accountStore.privateKey);
+          } catch (error) {
+            console.log(error);
+          }
+        }
+      }
     });
-  };
 
-  const handleClose = (event?: React.SyntheticEvent, reason?: string) => {
-    if (reason === "clickaway") {
-      return;
+    const handleSubmit = () => {
+      getDecreyptedKey({
+        variables: {
+          address: accountStore.selectedAddress,
+          passphrase: passphrase,
+        },
+      });
+    };
+
+    const handleClose = (event?: React.SyntheticEvent, reason?: string) => {
+      if (reason === "clickaway") {
+        return;
+      }
+
+      setSnackbarStatus(false);
+    };
+
+    // FIXME 키가 하나도 없을때 처리는 안해도 되지 않을지?
+    if (!accountStore.selectedAddress && accountStore.addresses.length > 0) {
+      accountStore.setSelectedAddress(accountStore.addresses[0]);
     }
 
-    setSnackbarStatus(false);
-  };
-
-  // FIXME 키가 하나도 없을때 처리는 안해도 되지 않을지?
-  if (!accountStore.selectedAddress && accountStore.addresses.length > 0) {
-    accountStore.setSelectedAddress(accountStore.addresses[0]);
-  }
-
-  return (
-    <div>
-      <form>
-        <FormControl>
-          <AccountSelect
-            addresses={accountStore.addresses}
-            onChangeAddress={accountStore.setSelectedAddress}
-            selectedAddress={accountStore.selectedAddress}
-          />
-        </FormControl>
+    return (
+      <div>
+        <form>
+          <FormControl>
+            <AccountSelect
+              addresses={accountStore.addresses}
+              onChangeAddress={accountStore.setSelectedAddress}
+              selectedAddress={accountStore.selectedAddress}
+            />
+          </FormControl>
+          <br />
+          <label>Passphrase</label>{" "}
+          <input
+            type="password"
+            onChange={(event) => {
+              setPassphrase(event.target.value);
+            }}
+            onKeyDown={(event) => {
+              // https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/keyCode
+              const enterKeyCode = 13;
+              if (enterKeyCode === event.keyCode) {
+                handleSubmit();
+              }
+            }}
+          ></input>
+        </form>
+        <button
+          disabled={loading}
+          onClick={(event) => {
+            handleSubmit();
+          }}
+        >
+          Login{" "}
+        </button>
         <br />
-        <label>Passphrase</label>{" "}
-        <input
-          type="password"
-          onChange={(event) => {
-            setPassphrase(event.target.value);
-          }}
-          onKeyDown={(event) => {
-            // https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/keyCode
-            const enterKeyCode = 13;
-            if (enterKeyCode === event.keyCode) {
-              handleSubmit();
-            }
-          }}
-        ></input>
-      </form>
-      <button
-        disabled={loading}
-        onClick={(event) => {
-          handleSubmit();
-        }}
-      >
-        Login{" "}
-      </button>
-      <br />
-      <button onClick={() => routerStore.push("/account")}>
-        {" "}
-        Account Management{" "}
-      </button>
-      <br />
-      <ClearCacheButton disabled={false} />
-      <br />
-      <button onClick={() => routerStore.push("/config")}> Config </button>
-      <Snackbar
-        open={openSnackbar}
-        autoHideDuration={3000}
-        onClose={handleClose}
-      >
-        <Alert onClose={handleClose} severity="error">
-          passphrase is wrong.
-        </Alert>
-      </Snackbar>
-    </div>
-  );
-});
+        <button onClick={() => routerStore.push("/account")}>
+          {" "}
+          Account Management{" "}
+        </button>
+        <br />
+        <ClearCacheButton disabled={false} />
+        <br />
+        <button onClick={() => routerStore.push("/config")}> Config </button>
+        <Snackbar
+          open={openSnackbar}
+          autoHideDuration={3000}
+          onClose={handleClose}
+        >
+          <Alert onClose={handleClose} severity="error">
+            passphrase is wrong.
+          </Alert>
+        </Snackbar>
+      </div>
+    );
+  }
+);
 
-export default inject("accountStore", "routerStore", "gameStore")(LoginView);
+export default inject(
+  "accountStore",
+  "routerStore",
+  "standaloneStore"
+)(LoginView);
--- src/renderer/views/login/MiningView.tsx
+++ src/renderer/views/login/MiningView.tsx
@@ -2,62 +2,46 @@ import * as React from "react";
 import { IStoreContainer } from "../../../interfaces/store";
 import { observer, inject } from "mobx-react";
 import { Button } from "@material-ui/core";
-import { standaloneProperties, LOCAL_SERVER_URL } from "../../../config";
 
-const MiningView = observer((props: IStoreContainer) => {
-  const { accountStore, routerStore, gameStore } = props;
-
-  const runStandalone = (isMining: boolean) => {
-    routerStore.push("/lobby");
-    const properties = {
-      ...standaloneProperties,
-      // Standalone에서는 마이닝을 안 할 거면 true를 주는 걸로 되어 있어서
-      // 인터페이스로 한 겹 감쌉니다.
-      NoMiner: !isMining,
-      PrivateKeyString: accountStore.privateKey,
+const MiningView = observer(
+  ({ accountStore, standaloneStore, routerStore }: IStoreContainer) => {
+    const runStandalone = (isMining: boolean) => {
+      standaloneStore.setMiner(!isMining);
+      routerStore.push("/lobby");
+      try {
+        standaloneStore.initStandalone(accountStore.privateKey);
+      } catch (error) {
+        console.log(error);
+      }
     };
 
-    fetch(`http://${LOCAL_SERVER_URL}/initialize-standalone`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(properties),
-    })
-      .then((response) => response.text())
-      .then((body) => console.log(body))
-      .then((_) =>
-        fetch(`http://${LOCAL_SERVER_URL}/run-standalone`, {
-          method: "POST",
-        })
-      )
-      .then((response) => response.text())
-      .then((body) => console.log(body))
-      .catch((error) => console.log(error));
-  };
+    return (
+      <div>
+        <label>Please turn on the mining option.</label>
+        <br />
+        <Button
+          variant="contained"
+          onClick={() => {
+            runStandalone(false);
+          }}
+        >
+          Not now
+        </Button>
+        <Button
+          variant="contained"
+          onClick={() => {
+            runStandalone(true);
+          }}
+        >
+          Got it!
+        </Button>
+      </div>
+    );
+  }
+);
 
-  return (
-    <div>
-      <label>Please turn on the mining option.</label>
-      <br />
-      <Button
-        variant="contained"
-        onClick={() => {
-          runStandalone(false);
-        }}
-      >
-        Not now
-      </Button>
-      <Button
-        variant="contained"
-        onClick={() => {
-          runStandalone(true);
-        }}
-      >
-        Got it!
-      </Button>
-    </div>
-  );
-});
-
-export default inject("accountStore", "routerStore", "gameStore")(MiningView);
+export default inject(
+  "accountStore",
+  "routerStore",
+  "standaloneStore"
+)(MiningView);
