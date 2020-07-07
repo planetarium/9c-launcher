19ba17e721f136e6a8b7cd54138c127f89df8784
Lee Suho <riemannulus@hitagi.moe>
2020-07-07 15:44:08 +0900

    삭제 아이콘 추가

--- src/renderer/components/ClearCacheButton.tsx
+++ src/renderer/components/ClearCacheButton.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 
 import { ipcRenderer } from "electron";
 import { Button, ButtonProps } from "@material-ui/core";
+import DeleteIcon from "@material-ui/icons/Delete";
 
 interface IClearCacheButtonProps extends ButtonProps {
   disabled?: boolean;
@@ -15,6 +16,7 @@ const ClearCacheButton = (props: IClearCacheButtonProps) => {
   return (
     <Button
       {...props}
+      startIcon={<DeleteIcon />}
       disabled={props.disabled}
       onClick={() => handleClick()}
       color="default"
