e8eef074b68ef2fdf0d81ffd14c653c2ed9972c0
Lee Suho <riemannulus@hitagi.moe>
2020-07-07 21:15:34 +0900

    마이닝 화면 목업 디자인

---
+++ src/renderer/views/login/MiningView.style.ts
@@ -0,0 +1,17 @@
+import { makeStyles } from "@material-ui/core";
+
+const miningViewStyle = makeStyles({
+  root: {
+    color: "white",
+  },
+
+  title: {
+    fontWeight: "bold",
+  },
+
+  button: {
+    borderRadius: "0",
+  },
+});
+
+export default miningViewStyle;
