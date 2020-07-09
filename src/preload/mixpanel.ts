0378adde0b0d2e7ecc51495384d44757759fb743
longfin <longfinfunnel@gmail.com>
2020-07-09 21:39:25 +0900

    mixpanel 설치 & 초기화

---
+++ src/preload/mixpanel.ts
@@ -0,0 +1,19 @@
+import isDev from "electron-is-dev";
+import mixpanel from "mixpanel-browser";
+import getMAC from "getmac";
+
+const token = "80a1e14b57d050536185c7459d45195a";
+
+export default function initializeMixpanel() {
+  if (isDev && false) {
+    console.debug("Mixpanel is disabled in development mode.");
+    return;
+  }
+
+  if (process.type !== "renderer") {
+    console.debug("Mixpanel is disabled in main process.");
+    return;
+  }
+
+  mixpanel.init(token);
+}
