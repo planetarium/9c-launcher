22db367152fc84037def4e8a36f6759e7aa7fc45
longfin <longfinfunnel@gmail.com>
2020-07-09 11:35:46 +0900

    재시작 버튼 추가

--- src/renderer/views/error/ErrorView.tsx
+++ src/renderer/views/error/ErrorView.tsx
@@ -1,12 +1,17 @@
 import * as React from "react";
+import { remote } from "electron";
 import { observer, inject } from "mobx-react";
 import { IStoreContainer } from "../../../interfaces/store";
 import { useProtectedPrivateKeysQuery } from "../../../generated/graphql";
 import errorViewStyle from "./ErrorView.style";
-import { Container, Typography } from "@material-ui/core";
+import { Button, Container, Typography } from "@material-ui/core";
 
 const ErrorView: React.FC<{}> = () => {
   const classes = errorViewStyle();
+  const handleRelaunch = React.useCallback(() => {
+    remote.app.relaunch();
+    remote.app.exit();
+  }, []);
   return (
     <Container className={classes.root}>
       <Typography variant="h1" gutterBottom className={classes.title}>
@@ -14,12 +19,18 @@ const ErrorView: React.FC<{}> = () => {
       </Typography>
       <Typography variant="subtitle1">Please follow step below.</Typography>
       <ol>
-        <li>Quit launcher.</li>
-        <li>Reopen launcher.</li>
-        <li>Clear Cache.</li>
-        <li>Download Snapshot.</li>
-        <li>Login.</li>
+        <li>Relaunch &ldquo;Nine Chornicles&rdquo;</li>
+        <li>Click Download Snapshot</li>
+        <li>Login once again</li>
       </ol>
+      <Button
+        color="primary"
+        variant="contained"
+        fullWidth
+        onClick={handleRelaunch}
+      >
+        Relaunch
+      </Button>
     </Container>
   );
 };
