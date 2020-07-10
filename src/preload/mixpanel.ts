149ee54df57497079d5e3aacd362641c3c1eff7e
longfin <longfinfunnel@gmail.com>
2020-07-10 11:56:51 +0900

    Mixpanel 오동작으로 개발환경에서 안켜지던 문제 우회

--- src/preload/mixpanel.ts
+++ src/preload/mixpanel.ts
@@ -5,15 +5,14 @@ import getMAC from "getmac";
 const token = "80a1e14b57d050536185c7459d45195a";
 
 export default function initializeMixpanel() {
-  if (isDev) {
-    console.debug("Mixpanel is disabled in development mode.");
-    return;
-  }
-
   if (process.type !== "renderer") {
     console.debug("Mixpanel is disabled in main process.");
     return;
   }
 
   mixpanel.init(token);
+  if (isDev) {
+    console.debug("Mixpanel is disabled in development mode.");
+    mixpanel.disable();
+  }
 }
