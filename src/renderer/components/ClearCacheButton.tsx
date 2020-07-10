799dea4b18931a9cf1d51dcb6f6126eb0138af33
Lee Suho <riemannulus@hitagi.moe>
2020-07-10 15:34:41 +0900

    클리어 캐시 버튼 피드백

--- src/renderer/components/ClearCacheButton.tsx
+++ src/renderer/components/ClearCacheButton.tsx
@@ -3,20 +3,24 @@ import * as React from "react";
 import { ipcRenderer } from "electron";
 import { Button, ButtonProps } from "@material-ui/core";
 import DeleteIcon from "@material-ui/icons/Delete";
+import CheckCircleIcon from "@material-ui/icons/CheckCircle";
 
 interface IClearCacheButtonProps extends ButtonProps {
   disabled?: boolean;
 }
 
 const ClearCacheButton = (props: IClearCacheButtonProps) => {
+  const [isCleared, setClearState] = React.useState(false);
+
   const handleClick = () => {
-    ipcRenderer.send("clear cache");
+    const result = ipcRenderer.sendSync("clear cache");
+    setClearState(result);
   };
 
   return (
     <Button
       {...props}
-      startIcon={<DeleteIcon />}
+      startIcon={isCleared ? <CheckCircleIcon /> : <DeleteIcon />}
       disabled={props.disabled}
       onClick={() => handleClick()}
       color="default"
