ac3624ff522d0bf90681fa874ceff21f75bf1ad4
Seunghun Lee <waydi1@gmail.com>
2020-08-12 19:47:21 +0900

    Build headless with version suffix

---
+++ scripts/build-headless.ps1
@@ -0,0 +1,7 @@
+#!/usr/bin/env pwsh
+dotnet publish NineChronicles.Standalone/NineChronicles.Standalone.Executable/NineChronicles.Standalone.Executable.csproj `
+  -c Release `
+  -r osx-x64 `
+  -o dist/publish `
+  --self-contained `
+  --version-suffix "$(git rev-parse HEAD)"
