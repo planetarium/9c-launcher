88d96ccd8e2091bc1150eae5e57eb71b37b0730f
Muhun Kim <iam@muhun.kim>
2020-08-14 13:35:07 +0900

    DownloadStore 는 존재하지 않은 스토어입니다
    사용하지 않아서 지웠습니다

--- src/interfaces/store.ts
+++ src/interfaces/store.ts
@@ -1,8 +1,6 @@
 import AccountStore from "../renderer/stores/account";
 import { RouterStore } from "mobx-react-router";
 import GameStore from "../renderer/stores/game";
-// @ts-ignore
-import DownloadStore from "../renderer/stores/download";
 import StandaloneStore from "../renderer/stores/standalone";
 
 export interface IStoreContainer {
