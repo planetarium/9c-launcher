0e39a3964fc8ca2aaed43beffdadd04d168dc768
Lee Suho <riemannulus@hitagi.moe>
2020-08-18 16:27:06 +0900

    DRY pattern
    Co-authored-by: Muhun Kim <iam@muhun.kim>

--- src/renderer/views/lobby/LobbyView.tsx
+++ src/renderer/views/lobby/LobbyView.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import {
-  Button,
+  Button as ButtonOrigin,
+  ButtonProps,
   Container,
   LinearProgress,
   TextField,
@@ -21,6 +22,10 @@ interface ILobbyViewProps extends IStoreContainer {
 function sleep(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
+
+const Button = (
+  props: Omit<ButtonProps, "fullWidth" | "variant" | "color">
+) => <ButtonOrigin fullWidth variant="contained" color="primary" {...props} />;
 
 const LobbyView = observer((props: ILobbyViewProps) => {
   const classes = lobbyViewStyle();
@@ -70,46 +75,42 @@ const LobbyView = observer((props: ILobbyViewProps) => {
     return false;
   }, [activatedError]);
 
-  if (loading || polling)
-    return <p className={classes.verifing}>Verifing...</p>;
-  if (!standaloneStore.IsPreloadEnded) return <PreloadWaitingButton />;
-  if (status?.activationStatus.activated) return <GameStartButton {...props} />;
-  else
-    return (
-      <Container>
-        <form onSubmit={handleActivateSubmit}>
-          <TextField
-            error={handleIsActivationSuccess()}
-            label="Activation Key"
-            onChange={privateKeyChangeHandle}
-            fullWidth
-          />
-          <Button
-            color="primary"
-            variant="contained"
-            className={classes.activation}
-            type="submit"
-          >
-            Activation
-          </Button>
-        </form>
-      </Container>
+  let child: JSX.Element;
+
+  if (loading || polling) {
+    child = <p className={classes.verifing}>Verifing...</p>;
+  } else if (!standaloneStore.IsPreloadEnded) {
+    child = <PreloadWaitingButton />;
+  } else if (status?.activationStatus.activated) {
+    child = <GameStartButton {...props} />;
+  } else {
+    child = (
+      <form onSubmit={handleActivateSubmit}>
+        <TextField
+          error={handleIsActivationSuccess()}
+          label="Activation Key"
+          onChange={privateKeyChangeHandle}
+          fullWidth
+        />
+        <ButtonOrigin
+          color="primary"
+          variant="contained"
+          className={classes.activation}
+          type="submit"
+        >
+          Activation
+        </ButtonOrigin>
+      </form>
     );
+  }
+  return <Container>{child}</Container>;
 });
 
 const PreloadWaitingButton = () => {
   return (
-    <Container>
-      <Button
-        fullWidth
-        disabled={true}
-        variant="contained"
-        color="primary"
-        className={lobbyViewStyle().gameStartButton}
-      >
-        Preloading...
-      </Button>
-    </Container>
+    <Button disabled={true} className={lobbyViewStyle().gameStartButton}>
+      Preloading...
+    </Button>
   );
 };
 
@@ -129,18 +130,13 @@ const GameStartButton = observer((props: ILobbyViewProps) => {
   }, [standaloneStore.IsPreloadEnded]);
 
   return (
-    <Container>
-      <Button
-        fullWidth
-        disabled={gameStore.isGameStarted}
-        variant="contained"
-        color="primary"
-        onClick={handleStartGame}
-        className={classes.gameStartButton}
-      >
-        {gameStore.isGameStarted ? "Now Running..." : "Start Game"}
-      </Button>
-    </Container>
+    <Button
+      disabled={gameStore.isGameStarted}
+      onClick={handleStartGame}
+      className={classes.gameStartButton}
+    >
+      {gameStore.isGameStarted ? "Now Running..." : "Start Game"}
+    </Button>
   );
 });
 
