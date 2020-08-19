9e38532b3eeb00298bd2005a3ad43bfe1770d3f7
Muhun Kim <iam@muhun.kim>
2020-08-19 10:11:14 +0900

    타입 체크 제외 주석을 추가합니다
    'IStoreContainer' 가 'Record<string, any>' 의 하위 타입이 아닙니다.

--- src/hooks/useStores.ts
+++ src/hooks/useStores.ts
@@ -3,6 +3,7 @@ import React from "react";
 import { MobXProviderContext } from "mobx-react";
 import { IStoreContainer } from "../interfaces/store";
 
+// @ts-ignore
 const storeContext: React.Context<IStoreContainer> = MobXProviderContext;
 
 export default function useStores() {
