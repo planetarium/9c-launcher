5cf4f2dd30316ef3c66d0b54a377392d25296776
Muhun Kim <iam@muhun.kim>
2020-08-25 17:40:48 +0900

    게임 실행 과정까지 E2E 테스트를 작성했습니다
    .env 파일을 만들고 PASSWORD 키값에 비밀번호를 적어주세요.
    
    예)
    PASSWORD=password

--- __tests__/main.spec.ts
+++ __tests__/main.spec.ts
@@ -1,14 +1,19 @@
 import path from "path";
 
-import { Application } from "spectron";
+import { Application, SpectronWebContents } from "spectron";
 import electron from "electron";
 
 import chai from "chai";
 import chaiAsPromised from "chai-as-promised";
 import assert from "assert";
 
+import "dotenv/config";
+
 // @ts-ignore
 process.env.ELECTRON_IS_DEV = 0;
+const { PASSWORD } = process.env;
+
+if (PASSWORD === undefined) throw Error("failed to load password from .env");
 
 chai.use(chaiAsPromised);
 
@@ -17,7 +22,7 @@ describe("test", function () {
 
   let app: Application;
 
-  beforeEach(function () {
+  before(function () {
     app = new Application({
       path: (electron as unknown) as string,
       args: [path.join(__dirname, "..", "dist")],
@@ -28,19 +33,48 @@ describe("test", function () {
   });
 
   afterEach(function () {
+    return app.client.waitUntil(
+      async function () {
+        const pathname = await app.webContents.executeJavaScript(
+          "location.pathname"
+        );
+        return typeof pathname === "string" && pathname !== "/error";
+      },
+      { timeoutMsg: "오류가 일어났습니다." }
+    );
+  });
+
+  it("로그인 하기", async function () {
+    this.timeout(1000);
+    const inputPassword = await app.client.$('input[type="password"]');
+    await inputPassword.setValue(PASSWORD);
+
+    const submitButton = await app.client.$('button[type="submit"]');
+    submitButton.click();
+  });
+
+  it("마이닝 끄기", async function () {
+    const miningOffButton = await app.client.$("#mining-off");
+    miningOffButton.click();
+  });
+
+  it("로비 뷰에서 실행 버튼 기다리기", async function () {
+    let submitButton = await app.client.$("body");
+
+    await app.client.waitUntil(
+      async function () {
+        submitButton = await app.client.$("#start-game");
+        const text = await submitButton.getText();
+        return text === "Start Game";
+      },
+      { timeout: 120000, timeoutMsg: "실행 버튼이 나오지 않았습니다." }
+    );
+    submitButton.click();
+  });
+
+  after(function () {
     if (app?.isRunning()) {
       app.mainProcess.exit(0);
     }
-  });
-
-  it("shows an initial window", async function () {
-    const count = await app.client.getWindowCount();
-    assert.strictEqual(count, 1);
-  });
-
-  it("capture log on main process", async function () {
-    await app.client.waitUntilWindowLoaded(10000);
-    const logs = await app.client.getMainProcessLogs();
-    logs.forEach(console.log);
   });
 });
