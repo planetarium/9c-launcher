a2650c1c77714092c51536e21492e1a98e26ae36
Lee Suho <riemannulus@hitagi.moe>
2020-08-28 13:52:44 +0900

    Add Settings button on Layout view

--- src/renderer/views/layout/Layout.tsx
+++ src/renderer/views/layout/Layout.tsx
@@ -3,11 +3,14 @@ import * as React from "react";
 import Button from "@material-ui/core/Button";
 import HomeIcon from "@material-ui/icons/Home";
 import DiscordIcon from "../../components/DiscordIcon";
+import SettingsIcon from "@material-ui/icons/Settings";
 import "../../styles/layout/layout.scss";
+import useStores from "../../../hooks/useStores";
 
 export interface ILayoutProps {}
 
 export const Layout: React.FC<ILayoutProps> = ({ children }) => {
+  const { routerStore } = useStores();
   return (
     <div className="layout">
       <div className="container">
@@ -31,6 +34,16 @@ export const Layout: React.FC<ILayoutProps> = ({ children }) => {
                 }}
               >
                 Discord
+              </Button>
+            </li>
+            <li>
+              <Button
+                startIcon={<SettingsIcon />}
+                onClick={() => {
+                  routerStore.push("/config");
+                }}
+              >
+                Settings
               </Button>
             </li>
           </ul>
