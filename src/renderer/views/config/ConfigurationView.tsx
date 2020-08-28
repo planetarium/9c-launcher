92327b7741fced2e7fe77f021ce73e15984609ea
Lee Suho <riemannulus@hitagi.moe>
2020-08-28 13:54:31 +0900

    Add store path configuration field

--- src/renderer/views/config/ConfigurationView.tsx
+++ src/renderer/views/config/ConfigurationView.tsx
@@ -1,26 +1,49 @@
 import * as React from "react";
-import { useState } from "react";
-import { LinearProgress, Button } from "@material-ui/core";
-import { observer, inject } from "mobx-react";
-import { IStoreContainer } from "../../../interfaces/store";
-import ClearCacheButton from "../../components/ClearCacheButton";
+import { Button, Container, TextField, FormLabel } from "@material-ui/core";
+import useStores from "../../../hooks/useStores";
+import { electronStore, blockchainStoreDirParent } from "../../../config";
+import { RootChainFormEvent } from "../../../interfaces/event";
 
-const ConfigurationView = observer(
-  ({ accountStore, routerStore }: IStoreContainer) => {
-    return (
-      <div>
-        <Button
-          onClick={() => routerStore.push("/")}
-          variant="contained"
-          color="primary"
-        >
-          Back to Home
-        </Button>
-        <br />
-        <br />
-      </div>
-    );
-  }
-);
+const ConfigurationView = () => {
+  const handleSubmit = (event: RootChainFormEvent) => {
+    event.preventDefault();
+    const rootChainPath = event.target.rootchain.value;
+    const chainDir = event.target.chain.value;
+    electronStore.set("BlockchainStoreDirParent", rootChainPath);
+    electronStore.set("BlockchainStoreDirName", chainDir);
+  };
 
-export default inject("accountStore", "routerStore")(ConfigurationView);
+  const { routerStore } = useStores();
+  return (
+    <div>
+      <Container>
+        <form onSubmit={handleSubmit}>
+          <FormLabel>Root chain store path</FormLabel>
+          <TextField
+            fullWidth
+            name="rootchain"
+            defaultValue={blockchainStoreDirParent}
+          />
+          <FormLabel>Chain store directory name</FormLabel>
+          <TextField
+            fullWidth
+            name="chain"
+            defaultValue={electronStore.get("BlockchainStoreDirName")}
+          />
+          <Button type="submit">OK</Button>
+        </form>
+      </Container>
+      <Button
+        onClick={() => routerStore.goBack()}
+        variant="contained"
+        color="primary"
+      >
+        Return
+      </Button>
+      <br />
+      <br />
+    </div>
+  );
+};
+
+export default ConfigurationView;
