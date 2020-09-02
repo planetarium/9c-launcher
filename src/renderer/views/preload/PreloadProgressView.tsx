f44e9ae08ee1a02696aad92f1ff85e0770959c1b
Lee Suho <riemannulus@hitagi.moe>
2020-09-02 13:09:00 +0900

    Fix useless state

--- src/renderer/views/preload/PreloadProgressView.tsx
+++ src/renderer/views/preload/PreloadProgressView.tsx
@@ -172,20 +172,6 @@ const PreloadProgressView = observer(() => {
   }, [preloadProgress?.extra]);
 
   React.useEffect(() => {
-    if (isPreloadEnded) {
-      const phase = preloadProgress?.extra.type;
-
-      if (
-        electronStore.get("PeerStrings").length > 0 &&
-        (phase === undefined ||
-          (phase !== "ActionExecutionState" && phase !== "StateDownloadState"))
-      ) {
-        gotoErrorPage("relaunch");
-      }
-    }
-  }, [isPreloadEnded, preloadProgress?.extra]);
-
-  React.useEffect(() => {
     if (preloadProgress !== undefined) {
       setStep(preloadProgress?.currentPhase + 2);
     }
