6b6c0684c3622f56c376acf58c8ae095a4f107b1
Muhun Kim <iam@muhun.kim>
2020-09-14 12:47:54 +0900

    Check if the route has been moved for E2E Test
    경로 이동을 명시적으로 태스트 되도록 수정했습니다.
    
    예시로, 틀린 비밀번호 입력한 경우 아래의 출력을 보게 됩니다.
    ```
      test
        √ 로그인 하기 (238ms)
        1) "after each" hook for "로그인 하기"
    
      1 passing (9s)
      1 failing
    
      1) test
           "after each" hook for "로그인 하기":
         Error: /login에서 다음 페이지로 이동에 실패했습니다.
    ```
    
    이전에는 요소의(WebdriverIO.Element) 텍스트, 클릭 메서드를 확인해서 이동을 했는지 암시적으로 추측 했습니다.
    
    그런데 클릭을 한 뒤여도 인증에 실패하는 등 아무런 반응이 없는 경우를 고려하지 못해서 수정했습니다.

--- __tests__/main.spec.ts
+++ __tests__/main.spec.ts
@@ -6,6 +6,8 @@ import electron from "electron";
 import "dotenv/config";
 import { expect } from "chai";
 
+const prefix = process.platform === "win32" ? "/C:" : "";
+
 // @ts-ignore
 process.env.ELECTRON_IS_DEV = 0;
 
@@ -17,6 +19,7 @@ describe("test", function () {
   this.timeout(10000);
 
   let app: Application;
+  let history = [`${prefix}/login`];
 
   before(function () {
     app = new Application({
@@ -26,17 +29,23 @@ describe("test", function () {
     return app.start();
   });
 
-  afterEach(function () {
-    return app.client.waitUntil(
-      async function () {
-        const pathname = await app.webContents.executeJavaScript(
-          "location.pathname"
-        );
-        console.log(pathname);
-        return typeof pathname === "string" && !pathname.includes("/error");
-      },
-      { timeoutMsg: "오류가 일어났습니다." }
+  afterEach(async function () {
+    const pathname = await app.webContents.executeJavaScript(
+      "location.pathname"
     );
+    if (typeof pathname !== "string")
+      throw Error("현재 경로를 가져오지 못했습니다");
+    if (pathname.includes("/error"))
+      throw Error("오류 페이지로 이동이 되었습니다");
+
+    if (history[history.length - 1] === pathname)
+      throw Error(
+        `"${
+          prefix === "/C:" ? pathname.slice(3) : pathname
+        }"에서 다음 페이지로 이동에 실패했습니다.`
+      );
+
+    history.push(pathname);
   });
 
   it("로그인 하기", async function () {
