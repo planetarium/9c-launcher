3091147f48287f6be4c6f196aec2e9fd43d79afe
Hyun Seungmin <boscohyun@gmail.com>
2021-03-19 12:48:35 +0900

    Use InternetConnectionStateView in Root

--- src/renderer/Root.tsx
+++ src/renderer/Root.tsx
@@ -25,36 +25,39 @@ import ErrorDownloadSnapshotMetadataFailedView from "./views/error/ErrorDownload
 import ErrorNoPermissionView from "./views/error/ErrorNoPermissionView";
 import ErrorReinstallView from "./views/error/ErrorReinstallView";
 import ErrorRelaunchView from "./views/error/ErrorRelaunchView";
+import InternetConnectionStateView from "./views/InternetConnectionStateView";
 
 class Root extends React.Component {
   render() {
     return (
       <Layout>
-        <Switch>
-          <Route exact path="/" component={IntroView} />
-          <Route exact path="/account" component={AccountView} />
-          <Route exact path="/account/create" component={CreateAccountView} />
-          <Route exact path="/account/create/copy" component={CopyCreatedPrivateKeyView} />
-          <Route exact path="/account/revoke" component={RevokeAccountView} />
-          <Route exact path="/account/reset/review-private-key" component={ReviewPrivateKeyView} />
-          <Route exact path="/account/reset/reset-password" component={ResetPasswordView} />
-          <Route exact path="/config" component={ConfigurationView} />
-          <Route exact path="/error/clear-cache" component={ErrorClearCacheView} />
-          <Route exact path="/error/disk-space" component={ErrorDiskSpaceView} />
-          <Route exact path="/error/download-binary-failed-error" component={ErrorDownloadBinaryFailedView} />
-          <Route exact path="/error/download-snapshot-failed-error" component={ErrorDownloadSnapshotFailedView} />
-          <Route exact path="/error/download-snapshot-metadata-failed-error" component={ErrorDownloadSnapshotMetadataFailedView} />
-          <Route exact path="/error/no-permission" component={ErrorNoPermissionView} />
-          <Route exact path="/error/reinstall" component={ErrorReinstallView} />
-          <Route exact path="/error/relaunch" component={ErrorRelaunchView} />
-          <Route exact path="/lobby" component={LobbyView} />
-          <Route exact path="/lobby/preload" component={PreloadView} />
-          <Route exact path="/login" component={LoginView} />
-          <Route exact path="/login/mining" component={MiningView} />
-          <Route exact path="/main" component={MainView} />
-          <Redirect from="*" to="/" />
-        </Switch>
-        <PreloadProgressView />
+        <InternetConnectionStateView>
+          <Switch>
+            <Route exact path="/" component={IntroView} />
+            <Route exact path="/account" component={AccountView} />
+            <Route exact path="/account/create" component={CreateAccountView} />
+            <Route exact path="/account/create/copy" component={CopyCreatedPrivateKeyView} />
+            <Route exact path="/account/revoke" component={RevokeAccountView} />
+            <Route exact path="/account/reset/review-private-key" component={ReviewPrivateKeyView} />
+            <Route exact path="/account/reset/reset-password" component={ResetPasswordView} />
+            <Route exact path="/config" component={ConfigurationView} />
+            <Route exact path="/error/clear-cache" component={ErrorClearCacheView} />
+            <Route exact path="/error/disk-space" component={ErrorDiskSpaceView} />
+            <Route exact path="/error/download-binary-failed-error" component={ErrorDownloadBinaryFailedView} />
+            <Route exact path="/error/download-snapshot-failed-error" component={ErrorDownloadSnapshotFailedView} />
+            <Route exact path="/error/download-snapshot-metadata-failed-error" component={ErrorDownloadSnapshotMetadataFailedView} />
+            <Route exact path="/error/no-permission" component={ErrorNoPermissionView} />
+            <Route exact path="/error/reinstall" component={ErrorReinstallView} />
+            <Route exact path="/error/relaunch" component={ErrorRelaunchView} />
+            <Route exact path="/lobby" component={LobbyView} />
+            <Route exact path="/lobby/preload" component={PreloadView} />
+            <Route exact path="/login" component={LoginView} />
+            <Route exact path="/login/mining" component={MiningView} />
+            <Route exact path="/main" component={MainView} />
+            <Redirect from="*" to="/" />
+          </Switch>
+          <PreloadProgressView />
+        </InternetConnectionStateView>
       </Layout>
     );
   }
