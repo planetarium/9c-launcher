25e427730fe85ce1a864345bfeec0bd8041ce442
longfin <longfinfunnel@gmail.com>
2021-03-29 13:25:06 +0900

    Fix private key validation
    Strip unexpected escapes

--- src/main/headless/validation.ts
+++ src/main/headless/validation.ts
@@ -1,10 +1,9 @@
-import { execSync } from "child_process";
 import { StandaloneSubcommand } from "./subcommand";
 
 export class Validation extends StandaloneSubcommand {
   public isValidPrivateKey(privateKeyHex: string): boolean {
     try {
-      this.execSync("validation", "private-key", `"${privateKeyHex}"`);
+      this.execSync("validation", "private-key", privateKeyHex);
       return true;
     } catch (error) {
       console.error(error);
