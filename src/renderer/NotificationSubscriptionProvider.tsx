de0086953b62e9b6446e180062e514f8366df402
Moreal <dev.moreal@gmail.com>
2021-03-29 11:54:22 +0900

    feat(graphql): use session

--- src/renderer/NotificationSubscriptionProvider.tsx
+++ src/renderer/NotificationSubscriptionProvider.tsx
@@ -1,56 +1,66 @@
+import { inject, observer } from "mobx-react";
 import React, { useEffect } from "react";
+import { IStoreContainer } from "src/interfaces/store";
 import {
   NotificationEnum,
   useNotificationSubscription,
 } from "../generated/graphql";
 
-export function NotificationSubscriptionProvider() {
-  const { loading, data } = useNotificationSubscription();
+const NotificationSubscriptionProvider = observer(
+  ({ accountStore }: IStoreContainer) => {
+    const { loading, data } = useNotificationSubscription();
 
-  useEffect(() => {
-    if (!loading && data) {
-      const { type, message } = data.notification;
-      let title = "";
-      let body = "";
+    useEffect(() => {
+      if (!loading && data) {
+        const { type, message, receiver } = data.notification;
+        let title = "";
+        let body = "";
 
-      if (type === NotificationEnum.Refill) {
-        title = "You can refill action point!";
-        body = "Turn on Nine Chronicles!";
-      }
+        if (receiver !== accountStore.selectedAddress) {
+          return;
+        }
 
-      if (type === NotificationEnum.Has) {
-        if (message) {
-          //stage id.
-          title = `Start Stage #${message}`;
+        if (type === NotificationEnum.Refill) {
+          title = "You can refill action point!";
           body = "Turn on Nine Chronicles!";
         }
-      }
 
-      if (type === NotificationEnum.Buyer) {
-        title = "Buy Item";
-        body = "Turn on Nine Chronicles!";
-      }
+        if (type === NotificationEnum.Has) {
+          if (message) {
+            //stage id.
+            title = `Start Stage #${message}`;
+            body = "Turn on Nine Chronicles!";
+          }
+        }
 
-      if (type === NotificationEnum.Seller) {
-        title = "Sell Item";
-        body = "Turn on Nine Chronicles!";
-      }
+        if (type === NotificationEnum.Buyer) {
+          title = "Buy Item";
+          body = "Turn on Nine Chronicles!";
+        }
 
-      if (type === NotificationEnum.CombinationConsumable) {
-        title = "Start Combination Consumable";
-        body = "Turn on Nine Chronicles!";
-      }
+        if (type === NotificationEnum.Seller) {
+          title = "Sell Item";
+          body = "Turn on Nine Chronicles!";
+        }
 
-      if (type === NotificationEnum.CombinationEquipment) {
-        title = "Start Combination Equipment";
-        body = "Turn on Nine Chronicles!";
-      }
+        if (type === NotificationEnum.CombinationConsumable) {
+          title = "Start Combination Consumable";
+          body = "Turn on Nine Chronicles!";
+        }
 
-      if (title && body) {
-        new Notification(title, { body: body });
-      }
-    }
-  }, [loading, data]);
+        if (type === NotificationEnum.CombinationEquipment) {
+          title = "Start Combination Equipment";
+          body = "Turn on Nine Chronicles!";
+        }
 
-  return <></>;
-}
+        if (title && body) {
+          new Notification(title, { body: body });
+        }
+      }
+    }, [loading, data, accountStore.privateKey]);
+
+    return <></>;
+  }
+);
+
+export default inject("accountStore")(NotificationSubscriptionProvider);
