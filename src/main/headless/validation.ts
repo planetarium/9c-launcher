b87ebb4c470f0b9dc9a3b9bb5eef7674b51996e6
Moreal <dev.moreal@gmail.com>
2021-03-29 13:21:17 +0900

    fix: correct private-key validation argument

--- src/main/headless/validation.ts
+++ src/main/headless/validation.ts
@@ -4,7 +4,7 @@ import { StandaloneSubcommand } from "./subcommand";
 export class Validation extends StandaloneSubcommand {
   public isValidPrivateKey(privateKeyHex: string): boolean {
     try {
-      this.execSync("validation", "private-key", `"${privateKeyHex}"`);
+      this.execSync("validation", "private-key", privateKeyHex);
       return true;
     } catch (error) {
       console.error(error);
