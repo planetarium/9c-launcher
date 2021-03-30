fbfb4012e1149c60085097eabfe0b24d0418e597
Hyun Seungmin <boscohyun@gmail.com>
2021-03-30 17:26:21 +0900

    Add back button to ReviewPrivateKeyView

--- src/renderer/views/account/reset/ReviewPrivateKeyView.tsx
+++ src/renderer/views/account/reset/ReviewPrivateKeyView.tsx
@@ -54,6 +54,14 @@ const ReviewPrivateKeyView: React.FC<IReviewPrivateKeyViewProps> = observer(
 
     return (
       <div className={classes.root}>
+        <Button
+          startIcon={<ArrowBackIosIcon />}
+          onClick={() => {
+            routerStore.push("/");
+          }}
+        >
+          {locale("뒤로")}
+        </Button>
         <Typography variant="h1" className={classes.title}>
           {locale("비밀번호를 재설정하기 위해 개인키를 입력해주세요.")}
         </Typography>
