e52d0240c8d33e9ebee5ed5eb238d3f54e7cc0fa
Moreal <dev.moreal@gmail.com>
2021-03-31 10:44:37 +0900

    test: complement unittest for Validation command

---
+++ __tests__/constant.ts
@@ -0,0 +1,9 @@
+import { join } from "path";
+
+export const HEADLESS_PATH = join(
+  __dirname,
+  "..",
+  "dist",
+  "publish",
+  "NineChronicles.Headless.Executable"
+);
---
+++ __tests__/main/headless/validation.spec.ts
@@ -0,0 +1,35 @@
+import { assert } from "chai";
+import { Validation } from "../../../src/main/headless/validation";
+import { HEADLESS_PATH } from "../../constant";
+
+describe("Validation", function () {
+  const validation = new Validation(HEADLESS_PATH);
+
+  describe("isValidPrivateKey", function () {
+    context("with invalid private key", function () {
+      it("It returns false with empty input", function () {
+        assert.isFalse(validation.isValidPrivateKey(""));
+      });
+
+      it("It returns false with odd-length input", function () {
+        assert.isFalse(validation.isValidPrivateKey("a"));
+        assert.isFalse(validation.isValidPrivateKey("abc"));
+        assert.isFalse(validation.isValidPrivateKey("1"));
+        assert.isFalse(validation.isValidPrivateKey("111"));
+      });
+    });
+
+    context("with valid private key", function () {
+      const validPrivateKeys = [
+        "ed5eba446d4c7cb50d7d5eb72c732773d89ee336f1880ee237b07589ed2387fc",
+        "243389a88fcb6956aa3fb23acd3b770fb05bcdb993b326c19b1e40918f03155a",
+        "11",
+      ];
+      validPrivateKeys.forEach((validPrivateKey) => {
+        it(`It returns false with "${validPrivateKey}"`, function () {
+          assert.isTrue(validation.isValidPrivateKey(validPrivateKey));
+        });
+      });
+    });
+  });
+});
