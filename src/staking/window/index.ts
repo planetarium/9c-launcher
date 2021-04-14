adb6eddc37b5f2ae166a58c0ae3b1effcccad8f0
Lee Suho <riemannulus@hitagi.moe>
2021-04-14 11:43:53 +0900

    feat: staking windows

---
+++ src/staking/window/index.ts
@@ -0,0 +1,31 @@
+import { BrowserWindow, app, shell } from "electron";
+import path from "path";
+import logoImage from "./resources/logo.png";
+import isDev from "electron-is-dev";
+
+const createStakingWindow = (): BrowserWindow => {
+  let _win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+    frame: true,
+    resizable: false,
+    autoHideMenuBar: true,
+    icon: path.join(app.getAppPath(), logoImage),
+  });
+
+  console.log(app.getAppPath());
+
+  if (isDev) {
+    _win.loadURL("http://localhost:9000/staking.html");
+    _win.webContents.openDevTools();
+  } else {
+    _win.loadFile("staking.html");
+  }
+
+  return _win;
+};
+
+export default createStakingWindow;
