88a46839155bf2e6c3d6e29baab8ba30699efaa4
Lee Suho <riemannulus@hitagi.moe>
2021-04-15 10:42:49 +0900

    feat: fix sass

---
+++ src/staking/components/Banner/BannerContainer.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import BannerComponent from "./BannerComponent"
+
+
+const BannerContainer: React.FC = () => {
+  return <BannerComponent />
+}
+
+export default BannerContainer
+
