8e53da51815cd3b7ff56207d468c10c88d96881c
Lee Suho <riemannulus@hitagi.moe>
2021-04-15 11:08:21 +0900

    add: storybook

---
+++ .storybook/preview.js
@@ -0,0 +1,29 @@
+const customViewports = {
+  kindleFire2: {
+    name: 'launcher v2',
+    styles: {
+      width: '800px',
+      height: '600px',
+    },
+  },
+  kindleFireHD: {
+    name: 'staking',
+    styles: {
+      width: '984px',
+      height: '552px',
+    },
+  },
+};
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+  viewport: {
+    viewports: customViewports
+  }
+}
