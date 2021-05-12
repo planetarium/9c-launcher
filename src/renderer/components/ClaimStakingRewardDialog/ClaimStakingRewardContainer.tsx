1dbf4aee48f8e4a747affbb663c9515c30ed9097
Lee Suho <riemannulus@hitagi.moe>
2021-05-12 13:51:35 +0900

    feat: claim staking reward component

--- src/renderer/components/ClaimStakingRewardDialog/ClaimStakingRewardContainer.tsx
+++ src/renderer/components/ClaimStakingRewardDialog/ClaimStakingRewardContainer.tsx
@@ -1,11 +1,40 @@
+import { Dialog } from "@material-ui/core"
 import React from "react"
+import useStores from "../../../hooks/useStores";
+import { useGetAvatarAddressQuery } from "../../../generated/graphql";
+import { Reward, RewardCategory } from "../../../staking/types";
+import ClaimStakingRewardDialog from "./ClaimStakingRewardDialog";
+
 
 type Props = {
-
+  open: boolean;
+  rewards: Reward[];
+  onActionTxId: (txId: string) => void;
 }
 
 const ClaimStakingRewardContainer: React.FC<Props> = (props: Props) => {
-  return <></>
+  const {open, rewards, onActionTxId} = props;
+  const {accountStore} = useStores();
+
+  const {loading, error, data} = useGetAvatarAddressQuery({variables: {
+    address: accountStore.selectedAddress,
+  }});
+
+  return (
+  <Dialog
+    disableBackdropClick
+    disableEscapeKeyDown
+    aria-labelledby="confirmation-dialog-title" open={open}>
+      {
+        data?.stateQuery.agent?.avatarStates 
+        ? <ClaimStakingRewardDialog
+        rewards={rewards}
+        avatar={data!.stateQuery.agent!.avatarStates!.map(x => {return {address: x.address, name: x.name}})}
+        onActionTxId={onActionTxId}/>
+        : <div>You need create avatar first</div>
+      }
+
+  </Dialog>)
 }
 
 export default ClaimStakingRewardContainer
