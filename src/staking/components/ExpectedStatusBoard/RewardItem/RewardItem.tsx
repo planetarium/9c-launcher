b0165108913a99dea72be1d19be46c22359be823
Lee Suho <riemannulus@hitagi.moe>
2021-05-13 13:21:16 +0900

    style: ExpectedStatusBoard

--- src/staking/components/ExpectedStatusBoard/RewardItem/RewardItem.tsx
+++ src/staking/components/ExpectedStatusBoard/RewardItem/RewardItem.tsx
@@ -1,22 +1,37 @@
 import React from "react";
-import { RewardCategory } from "src/staking/types";
+import { RewardCategory } from "../../../../staking/types";
+import goldIcon from '../../../common/resources/gold.png';
+import hourglass from '../../../common/resources/hourglass.png';
+import apIcon from '../../../common/resources/ap.png';
 
 export type Props = {
-  item: RewardCategory
+  item: RewardCategory | "GOLD"
   left: number
   right: number
+}
+
+
+const getIconFromItem = (item: RewardCategory | "GOLD") => {
+  switch(item) {
+    case RewardCategory.AP:
+      return apIcon;
+    case RewardCategory.HOURGLASS:
+      return hourglass;
+    case "GOLD":
+      return goldIcon;
+    default:
+      return null
+  }
 }
 
 const RewardItem: React.FC<Props> = (props: Props) => {
   const {item, left, right} = props;
   return <div>
+    <img src={getIconFromItem(item) || ""} />
     <div>
     {
       left === right ? left : `${left} -> ${right}`
     }
-    </div>
-    <div>
-      image
     </div>
   </div>
 }
