e8ae9ba93125b2aa459df8276514c3c1880a7b1a
Lee Suho <riemannulus@hitagi.moe>
2021-05-20 17:15:14 +0900

    fix: code styles

--- src/collection/pages/landing/landing.tsx
+++ src/collection/pages/landing/landing.tsx
@@ -8,7 +8,7 @@ export type Props = {
 }
 
 const Landing: React.FC<Props> = (props: Props) => {
-  const {handleNextButton} = props;
+  const { handleNextButton } = props;
 
   const gotoIntro = () => handleNextButton();
   return (
@@ -17,24 +17,24 @@ const Landing: React.FC<Props> = (props: Props) => {
       style={{ backgroundImage: `url(${BackgroundImage})` }}
     >
       <div className={'IntroPageBorder'}>
-      <div className={"IntroItems"}>
-      <div className={"Title"}>
-        Collect cute monsters! Then, you can earn special rewards!
+        <div className={"IntroItems"}>
+          <div className={"Title"}>
+            Collect cute monsters! Then, you can earn special rewards!
       </div>
-      <div className={"Subtitle"}>
-        NCG is required to collect monsters. You can get rewards as many monsters as you collect.
+          <div className={"Subtitle"}>
+            NCG is required to collect monsters. You can get rewards as many monsters as you collect.
       </div>
-      <div className={"Button"} >
-      <CollectionButton
-      width={240}
-      height={66}
-      primary={true}
-        onClick={() => {gotoIntro();}}
-      >
-        Start
+          <div className={"Button"} >
+            <CollectionButton
+              width={240}
+              height={66}
+              primary={true}
+              onClick={() => { gotoIntro(); }}
+            >
+              Start
       </CollectionButton>
-      </div>
-      </div>
+          </div>
+        </div>
       </div>
 
 
