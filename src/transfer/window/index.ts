2002876aee1a3c76dfc930eaa96a1a11bf954fa3
Lee Suho <riemannulus@hitagi.moe>
2021-07-27 18:07:54 +0900

    feat(transfer): bolierplate for new window

---
+++ src/transfer/window/index.ts
@@ -0,0 +1,31 @@
+import { BrowserWindow, app, shell, ipcMain } from "electron";
+import path from "path";
+import logoImage from "./resources/logo.png";
+import isDev from "electron-is-dev";
+
+let _win: BrowserWindow | null = null;
+
+const createCollectionWindow = (): BrowserWindow => {
+  _win = new BrowserWindow({
+    width: 1166,
+    height: 738,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+    frame: true,
+    resizable: false,
+    autoHideMenuBar: true,
+    icon: path.join(app.getAppPath(), logoImage),
+  });
+
+  if (isDev) {
+    _win.loadURL("http://localhost:9000/transfer.html");
+    _win.webContents.openDevTools();
+  } else {
+    _win.loadFile("transfer.html");
+  }
+
+  return _win;
+};
+
+export default createCollectionWindow;
