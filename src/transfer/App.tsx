d1af75f59956386b6a6d33291b3e7e364acb48aa
Lee Suho <riemannulus@hitagi.moe>
2021-07-28 08:56:38 +0900

    refactor(transfer): do not use apollo on transfer

--- src/transfer/App.tsx
+++ src/transfer/App.tsx
@@ -1,50 +1,9 @@
-import { InMemoryCache } from "apollo-cache-inmemory";
-import ApolloClient from "apollo-client";
-import { split, ApolloLink } from "apollo-link";
-import { createHttpLink } from "apollo-link-http";
-import { RetryLink } from "apollo-link-retry";
-import { WebSocketLink } from "apollo-link-ws";
 import React from "react";
-import { LOCAL_SERVER_URL } from "../config";
-import './App.scss';
-import { getMainDefinition } from "apollo-utilities";
-import { ApolloProvider } from "react-apollo";
 
-
-const wsLink = new WebSocketLink({
-  uri: `ws://${LOCAL_SERVER_URL}/graphql`,
-  options: {
-    reconnect: true,
-  },
-});
-
-const httpLink = createHttpLink({ uri: `http://${LOCAL_SERVER_URL}/graphql` });
-
-const apiLink = split(
-  // split based on operation type
-  ({ query }) => {
-    const definition = getMainDefinition(query);
-    return (
-      definition.kind === "OperationDefinition" &&
-      definition.operation === "subscription"
-    );
-  },
-  wsLink,
-  httpLink
-);
-
-
-const link = ApolloLink.from([new RetryLink(), apiLink]);
-
-const client = new ApolloClient({
-  link: link,
-  cache: new InMemoryCache(),
-});
 
 const App: React.FC = () => {
   return (
-  <ApolloProvider client={client}>
-  </ApolloProvider>
+    <></>
   )
 };
 
