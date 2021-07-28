a4b6492cf9cf21d825ae4217995040639a315d2f
Lee Suho <riemannulus@hitagi.moe>
2021-07-28 18:08:39 +0900

    feat(transfer): menu store

---
+++ src/transfer/stores/menu.ts
@@ -0,0 +1,23 @@
+import { ipcRenderer } from "electron";
+import { observable, action, decorate } from "mobx";
+import headlessGraphQLSDK, { GraphQLSDK } from "../middleware/graphql";
+
+export enum MenuItems {
+  TRANSFER,
+  EXCHANGE,
+}
+
+export interface IMenuStore {
+  currentMenu: MenuItems;
+}
+
+
+export default class MenuStore implements IMenuStore {
+  @observable
+  public currentMenu: MenuItems = MenuItems.TRANSFER;
+
+  @action
+  changeMenu(menu: MenuItems) {
+    this.currentMenu = menu;
+  }
+}
