30839bab81bc397cd880a1c1e62900c7503077d7
Lee Suho <riemannulus@hitagi.moe>
2021-07-28 16:12:24 +0900

    feat(transfer): implement som method

--- src/transfer/stores/transfer.ts
+++ src/transfer/stores/transfer.ts
@@ -21,6 +21,11 @@ export default class TransferStore implements ITransferStore {
   }
 
   @action
+  getAgentAddress = () => {
+    return this.agentAddress;
+  }
+
+  @action
   trySetAgentAddress = async (): Promise<boolean> => {
     const minerAddress = await this.graphqlSdk.MinerAddress();
     if (minerAddress.data) {
@@ -33,13 +38,21 @@ export default class TransferStore implements ITransferStore {
 
   @action
   transferGold = async (recipient: string, amount: number, memo: string): Promise<boolean> => {
+    this.checkAgentAddress();
+
+    const nextTxNonceData = await this.graphqlSdk.GetNextTxNonce({address: this.agentAddress});
+    if (!nextTxNonceData.data) {
+      throw new Error("Failed to get next nonce");
+    }
+    const txNonce = nextTxNonceData.data.transaction.nextTxNonce as string;
+    const tx = await this.graphqlSdk.Transfer({recipient, amount: amount.toString(), memo, txNonce});
 
     return false;
   }
 
   @action
   swapToWNCG = async (recipient: string, amount: number): Promise<boolean> => {
-    const bridgeAddress = "";
-    return this.transferGold(bridgeAddress, amount, recipient);
+    const bridgeAddress = "0xa208a3E10964dd8bB044a87a31967bafd9458907"; // testnet
+    return await this.transferGold(bridgeAddress, amount, recipient);
   }
 }
