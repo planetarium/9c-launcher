71a8287347acba8a3473a69ce91f0e2248fa622b
Basix <basix@basix.tech>
2021-07-29 21:11:01 +0900

    Use react-hot-loader

--- src/v2/App.tsx
+++ src/v2/App.tsx
@@ -1,9 +1,11 @@
+import { hot } from "react-hot-loader";
 import { ApolloProvider } from "@apollo/client";
 import React from "react";
 import { HashRouter as Router } from "react-router-dom";
 import Routes from "./Routes";
 import client from "./utils/apolloClient";
 import APVSubscriptionProvider from "./utils/APVSubscriptionProvider";
+import "./global.scss";
 
 function App() {
   return (
@@ -17,4 +19,4 @@ function App() {
   );
 }
 
-export default App;
+export default hot(module)(App);
