1a6e7e861910c3de8823a03028371447b9a359d8
Lee Suho <riemannulus@hitagi.moe>
2021-07-30 13:51:11 +0900

    feat(transfer): confirm transaction

--- src/transfer/stores/transfer.ts
+++ src/transfer/stores/transfer.ts
@@ -1,10 +1,12 @@
 import { ipcRenderer } from "electron";
 import { observable, action, decorate } from "mobx";
+import { sleep } from "src/utils";
 import headlessGraphQLSDK, { GraphQLSDK } from "../middleware/graphql";
 
 export interface ITransferStore {
 }
 
+type TxExecutionCallback = (blockIndex: number, blockHash: string) => void;
 
 export default class TransferStore implements ITransferStore {
   private agentAddress: string = "";
@@ -47,7 +49,7 @@ export default class TransferStore implements ITransferStore {
   };
 
   @action
-  transferGold = async (recipient: string, amount: number, memo: string): Promise<boolean> => {
+  transferGold = async (recipient: string, amount: number, memo: string): Promise<string> => {
     this.assertAgentAddress();
 
     const nextTxNonceData = await this.graphqlSdk.GetNextTxNonce({address: this.agentAddress});
@@ -56,14 +58,58 @@ export default class TransferStore implements ITransferStore {
     }
     const txNonce = nextTxNonceData.data.transaction.nextTxNonce as string;
     const tx = await this.graphqlSdk.Transfer({recipient, amount: amount.toString(), memo, txNonce});
+    if (!tx.data) {
+      throw new Error(`Failed to create transaction. ${tx.errors}`);
+    }
 
-    return false;
+    return tx.data.transfer;
   }
 
   @action
-  swapToWNCG = async (recipient: string, amount: number): Promise<boolean> => {
+  swapToWNCG = async (recipient: string, amount: number): Promise<string> => {
     const bridgeAddress = "0xa208a3E10964dd8bB044a87a31967bafd9458907"; // testnet
     return await this.transferGold(bridgeAddress, amount, recipient);
+  }
+
+  @action
+  confirmTransaction = async (
+    txId: string,
+    timeout: number | undefined,
+    onSuccess: TxExecutionCallback,
+    onFailure: TxExecutionCallback,
+    onTimeout: TxExecutionCallback): Promise<void> => {
+    const txStatus = await this.graphqlSdk.TransactionResult({txId});
+    if (!txStatus.data) {
+      throw new Error("Failed to get transaction status");
+    }
+
+    const startTime = Date.now();
+
+    let txResult = txStatus.data.transaction.transactionResult;
+    while(true) {
+      switch(txResult.txStatus) {
+        case "SUCCESS":
+          onSuccess(txResult.blockIndex, txResult.blockHash as string);
+          return;
+        case "FAILURE":
+          onFailure(txResult.blockIndex, txResult.blockHash as string);
+          return;
+        case "INVALID":
+        case "STAGNING":
+          if(timeout) {
+            const elapsed = Date.now() - startTime;
+            if(elapsed >= timeout) {
+              onTimeout(txResult.blockIndex, txResult.blockHash as string);
+              return;
+            }
+          }
+          txResult = await (await this.graphqlSdk.TransactionResult({txId})).data!.transaction.transactionResult;
+          break;
+        default:
+          throw new Error(`Unknown transaction status: ${txResult.txStatus}`);
+      }
+      await sleep(1000);
+    }
   }
   
   @action 
