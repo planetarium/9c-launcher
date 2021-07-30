c37a7477837ca7d90e46d17f6cc32f8dbb805631
Lee Suho <riemannulus@hitagi.moe>
2021-07-30 18:29:10 +0900

    feat(transfer): introduce TransferPageStore

---
+++ src/transfer/stores/views/transfer.ts
@@ -0,0 +1,85 @@
+import { action, observable } from "mobx";
+
+export enum TransferPhase {
+  READY,
+  SENDTX,
+  SENDING,
+  FINISHED,
+}
+
+export interface ITransferPageStore {
+  recipient: string;
+  memo: string;
+  tx: string;
+  amount: number;
+  success: boolean;
+  currentPhase: TransferPhase;
+}
+
+
+export default class TransferPageStore implements ITransferPageStore {
+  @observable public recipient: string;
+  @observable public memo: string;
+  @observable public tx: string;
+  @observable public amount: number;
+  @observable public success: boolean;
+  @observable public currentPhase: TransferPhase;
+
+  constructor() {
+    this.recipient = "";
+    this.memo = "";
+    this.tx = "";
+    this.amount = -1;
+    this.success = false;
+
+    this.currentPhase = TransferPhase.READY;
+  }
+
+  @action
+  public setRecipient(recipient: string) {
+    this.recipient = recipient;
+  }
+
+  @action
+  public setMemo(memo: string) {
+    this.memo = memo;
+  }
+
+  @action
+  public setAmount(amount: number) {
+    this.amount = amount;
+  }
+
+  /*
+    This state machine is a simple.
+    startSend() -> setTx() -> endSend() -> finish()
+    FIXME: this state machine do not have any error handling.
+  */
+  @action
+  public startSend() {
+    this.currentPhase = TransferPhase.SENDTX;
+  }
+
+  @action
+  public setTx(tx: string) {
+    this.tx = tx;
+    this.currentPhase = TransferPhase.SENDING;
+  }
+
+  @action
+  public endSend(success: boolean) {
+    this.success = success;
+    this.currentPhase = TransferPhase.FINISHED;
+  }
+
+  @action
+  public finish() {
+    this.currentPhase = TransferPhase.READY;
+    this.success = false;
+  }
+
+  @action
+  public validateRecipient(): boolean {
+    return /^0x[0-9a-fA-F]{40}$/.test(this.recipient);
+  }
+}
