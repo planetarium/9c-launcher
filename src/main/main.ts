de701e26d1c848603d22cb6a4ffb090dc41214ba
Basix <basix@basix.tech>
2021-08-12 17:35:42 +0900

    Fix transfer window devtools

--- src/main/main.ts
+++ src/main/main.ts
@@ -478,7 +478,7 @@ function initializeIpc() {
       collectionWin.focus();
       return;
     }
-    collectionWin = createTransferWindow();
+    collectionWin = await createTransferWindow();
     collectionWin.on("close", function (event: any) {
       collectionWin = null;
     });
