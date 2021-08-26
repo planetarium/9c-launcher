26d58caeebf475a4d20f720d87e8c9c36b168c48
Basix <basix@basix.tech>
2021-08-26 21:03:39 +0900

    feat(RegisterView): add TextField samples

--- src/v2/views/RegisterView/index.tsx
+++ src/v2/views/RegisterView/index.tsx
@@ -1,8 +1,16 @@
 import React from "react";
 import { observer } from "mobx-react";
+import Layout from "src/v2/components/core/Layout";
+import TextField from "src/v2/components/ui/TextField";
+import styles from "./styles.module.scss";
 
 function RegisterView() {
-  return null;
+  return (
+    <Layout sidebar className={styles.register}>
+      <h1>Register</h1>
+      <TextField type="text" label="ID" />
+    </Layout>
+  );
 }
 
 export default observer(RegisterView);
