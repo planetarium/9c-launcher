661fc6cd85686d82ca46103f2ffd518722560748
Yang Chun Ung <qooraven@gmail.com>
2021-09-23 16:45:19 +0900

    Fix timezone

--- __tests__/main/headless/tx.spec.ts
+++ __tests__/main/headless/tx.spec.ts
@@ -10,7 +10,7 @@ describe("Tx", function () {
   describe("Sign", function () {
     const privateKey = "9c1c8da789b5c83f459c9d5034446af4a050ee14117c2a68d2439a3ed3b98f0a";
     const genesisHash = "4582250d0da33b06779a8475d283d5dd210c683b9b999d74d03fac4f58fa6bce";
-    const timeStamp = "2021-09-14T09:00:00";
+    const timeStamp = "2021-09-14T00:00:00Z";
     context("without action", function () {
       it("It returns error", function () {
         let sign = tx.Sign(privateKey, 1, genesisHash, new Date(timeStamp).toISOString(), "");
