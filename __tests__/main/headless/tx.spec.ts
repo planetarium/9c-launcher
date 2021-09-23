358a4d74ebd65d4d9acc95e34f992ebc5d06ac22
Yang Chun Ung <qooraven@gmail.com>
2021-09-23 11:22:54 +0900

    using Date instead of moment

--- __tests__/main/headless/tx.spec.ts
+++ __tests__/main/headless/tx.spec.ts
@@ -10,10 +10,10 @@ describe("Tx", function () {
   describe("Sign", function () {
     const privateKey = "9c1c8da789b5c83f459c9d5034446af4a050ee14117c2a68d2439a3ed3b98f0a";
     const genesisHash = "4582250d0da33b06779a8475d283d5dd210c683b9b999d74d03fac4f58fa6bce";
-    const timeStamp = "2021-09-14T00:00:00";
+    const timeStamp = "2021-09-14T09:00:00";
     context("without action", function () {
       it("It returns error", function () {
-        let sign = tx.Sign(privateKey, 1, genesisHash, timeStamp, "");
+        let sign = tx.Sign(privateKey, 1, genesisHash, new Date(timeStamp).toISOString(), "");
         assert.isNotEmpty(sign.stderr);
         assert.isEmpty(sign.stdout)
       });
@@ -22,7 +22,7 @@ describe("Tx", function () {
       const actionPath = path.join(__dirname, "..", "..", "fixture", "activate_account.txt");
       const txPath = path.join(__dirname, "..", "..", "fixture", "tx_activate_account.txt");
       it("It returns dumped tx", function () {
-        let sign = tx.Sign(privateKey, 1, genesisHash, timeStamp, actionPath);
+        let sign = tx.Sign(privateKey, 1, genesisHash, new Date(timeStamp).toISOString(), actionPath);
         assert.isNotEmpty(sign.stdout);
         assert.isEmpty(sign.stderr)
         let result = sign.stdout;
