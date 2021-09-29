3ffd091ab2c47e4950fa334c91edacfd97390f9f
Basix <basix@basix.tech>
2021-09-29 16:45:37 +0900

    Add partial LoginView implementation
    This implementation covers the single account case only

--- src/v2/views/LoginView/index.tsx
+++ src/v2/views/LoginView/index.tsx
@@ -1,9 +1,80 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { observer } from "mobx-react";
 import Layout from "../../components/core/Layout";
+import { useStore } from "src/v2/utils/useStore";
+import { ipcRenderer } from "electron";
+import { useHistory } from "react-router";
+import { CSS } from "src/v2/stitches.config";
+
+import H1 from "src/v2/components/ui/H1";
+import TextField from "src/v2/components/ui/TextField";
+import Button from "src/v2/components/ui/Button";
+
+const LoginStyles: CSS = {
+  padding: 52,
+  boxSizing: "border-box",
+  display: "flex",
+  flexDirection: "column",
+  "& > * + *": {
+    marginTop: 16,
+  },
+  height: "100%",
+  marginBottom: 52,
+};
 
 function LoginView() {
-  return <Layout sidebar />;
+  const { account } = useStore();
+  const [password, setPassword] = useState("");
+  const [invalid, setInvalid] = useState(false);
+  const history = useHistory();
+
+  const handleLogin = () => {
+    const [unprotectedPrivateKey, error] = ipcRenderer.sendSync(
+      "unprotect-private-key",
+      account.selectedAddress,
+      password
+    );
+    if (error !== undefined) {
+      setInvalid(true);
+      ipcRenderer.send("mixpanel-track-event", "Launcher/LoginFailed");
+    }
+
+    if (unprotectedPrivateKey !== undefined) {
+      account.setPrivateKey(unprotectedPrivateKey);
+      account.setLoginStatus(true);
+      ipcRenderer.send("mixpanel-alias", account.selectedAddress);
+      ipcRenderer.send("mixpanel-track-event", "Launcher/Login");
+      history.push("/lobby");
+    }
+  };
+
+  useEffect(() => {
+    account.setSelectedAddress(account.addresses[0]); // Testing
+  }, [account]);
+
+  return (
+    <Layout sidebar css={LoginStyles}>
+      <H1>Login</H1>
+      <p>Welcome back Nine Chronicles!</p>
+      <TextField label="ID" readOnly value={account.selectedAddress} />
+      <TextField
+        label="Password"
+        type="password"
+        value={password}
+        invalid={invalid}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <div>
+        <Button
+          variant="primary"
+          onClick={handleLogin}
+          css={{ width: 280, marginTop: "auto", alignSelf: "center" }}
+        >
+          LOGIN
+        </Button>
+      </div>
+    </Layout>
+  );
 }
 
 export default observer(LoginView);
