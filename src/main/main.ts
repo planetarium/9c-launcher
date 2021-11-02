7c678c2c5367fe87ccdc661c4e7eb0f7f7190d1f
area363 <kidonseo@gmail.com>
2021-11-02 18:19:48 +0900

    save signer address for transfer page

--- src/main/main.ts
+++ src/main/main.ts
@@ -488,12 +488,15 @@ function initializeIpc() {
     });
   });
 
-  ipcMain.handle("open transfer page", async () => {
+  ipcMain.handle("open transfer page", async (_, selectedAddress) => {
     if (collectionWin != null) {
       collectionWin.focus();
       return;
     }
+    console.log(`open transfer page address: ${selectedAddress}`);
     collectionWin = await createTransferWindow();
+    console.log(`call set miner address: ${selectedAddress}`);
+    collectionWin!.webContents.send("set miner address", selectedAddress);
     collectionWin.on("close", function (event: any) {
       collectionWin = null;
     });
