5fe83f011e54e81ff427d8dfc6719ef7bf9054fd
Basix <basix@basix.tech>
2021-11-25 17:27:44 +0900

    chore: encode .env as utf8 when creating key

--- scripts/create-key.ps1
+++ scripts/create-key.ps1
@@ -1,4 +1,4 @@
 $passphrase = Get-Random
 $privateKey = "$(npx planet key generate -A)"
 npx planet key import --passphrase="$passphrase" "$privateKey"
-Write-Output "PASSWORD=$passphrase" | Out-File -FilePath .env
+Write-Output "PASSWORD=$passphrase" | Out-File -FilePath .env -Encoding UTF8
