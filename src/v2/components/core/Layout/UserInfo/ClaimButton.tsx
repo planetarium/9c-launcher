ee9ea8b0f79d968f580735f33013fb0e188eca3c
Basix <basix@basix.tech>
2022-03-10 14:34:04 +0900

    Prevent ClaimButton to open monster collection

--- src/v2/components/core/Layout/UserInfo/ClaimButton.tsx
+++ src/v2/components/core/Layout/UserInfo/ClaimButton.tsx
@@ -1,11 +1,12 @@
 import { motion } from "framer-motion";
-import React from "react";
+import React, { MouseEvent, useCallback } from "react";
 import { T } from "src/renderer/i18n";
 import { styled } from "src/v2/stitches.config";
 
 const Button = styled(motion.button, {
   appearance: "none",
   backgroundColor: "#dc9c2d",
+  border: "none",
   padding: "5px 1rem",
   "&:disabled": {
     backgroundColor: "$gray",
@@ -18,8 +19,13 @@ interface ClaimButtonProps {
 }
 
 export function ClaimButton({ loading, onClick }: ClaimButtonProps) {
+  const eventListener = useCallback<(e: MouseEvent) => void>((e) => {
+    e.stopPropagation();
+    onClick();
+  }, [onClick])
+
   return (
-    <Button disabled={loading} onClick={onClick}>
+    <Button disabled={loading} onClick={eventListener}>
       <T _str="Get Rewards" _tags="v2/ClaimButton" />
     </Button>
   );
