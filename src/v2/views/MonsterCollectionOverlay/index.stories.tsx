2e1ba3d33607859529c417c3482867dac6e1e37c
Basix <basix@basix.tech>
2022-05-16 20:47:53 +0900

    Update stories to mock GraphQL requests

--- src/v2/views/MonsterCollectionOverlay/index.stories.tsx
+++ src/v2/views/MonsterCollectionOverlay/index.stories.tsx
@@ -1,6 +1,17 @@
 import React from "react";
+import { MockedProvider, MockedResponse } from "@apollo/client/testing";
+
 import { MonsterCollectionContent } from ".";
 import { MonsterCollectionOverlayBase } from "./base";
+
+import {
+  CurrentStakingDocument,
+  CurrentStakingQuery,
+  StakingSheetDocument,
+  StakingSheetQuery,
+  useCurrentStakingQuery,
+  useStakingSheetQuery,
+} from "src/v2/generated/graphql";
 
 import "normalize.css";
 
@@ -9,8 +20,67 @@ export default {
   component: MonsterCollectionContent,
 };
 
+function MonsterCollectionOverlay() {
+  const { data: sheet } = useStakingSheetQuery();
+  const { data: current } = useCurrentStakingQuery();
+
+  if (!sheet || !current) return null;
+
+  return (
+    <MonsterCollectionOverlayBase as="main">
+      <MonsterCollectionContent sheet={sheet} current={current} />
+    </MonsterCollectionOverlayBase>
+  );
+}
+
+const mocks: [
+  MockedResponse<CurrentStakingQuery>,
+  MockedResponse<StakingSheetQuery>
+] = [
+  {
+    request: { query: CurrentStakingDocument },
+    result: {
+      data: {
+        stateQuery: {
+          stakeState: {
+            deposit: 500,
+            cancellableBlockIndex: 0,
+            claimableBlockIndex: 50400,
+            receivedBlockIndex: 0,
+            startedBlockIndex: 0,
+          },
+        },
+      },
+    },
+  },
+  {
+    request: { query: StakingSheetDocument },
+    result: {
+      data: {
+        stateQuery: {
+          stakeRegularRewardSheet: {
+            orderedList: [
+              // FIXME Change the value to something realistic
+              {
+                level: 1,
+                requiredGold: 10,
+                rewards: [
+                  {
+                    itemId: 191,
+                    rate: 20,
+                  },
+                ],
+              },
+            ],
+          },
+        },
+      },
+    },
+  },
+];
+
 export const FirstPage = () => (
-  <MonsterCollectionOverlayBase as="main">
-    <MonsterCollectionContent />
-  </MonsterCollectionOverlayBase>
+  <MockedProvider mocks={mocks}>
+    <MonsterCollectionOverlay />
+  </MockedProvider>
 );
