e75043ea3895af0ca2565975d27ed6577b376034
Basix <basix@basix.tech>
2022-08-11 17:31:28 +0900

    Ignore the postinstall if it's CI

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+if [ -z "$CI" ]; then
+  exit 0
+fi
+
 [[ -d dist/publish && -d NineChronicles.Headless ]] || yarn build-headless
 [[ -d 9c_Data && -d NineChronicles ]] || yarn bundle-player
 [[ -d src/generated ]] || yarn codegen
