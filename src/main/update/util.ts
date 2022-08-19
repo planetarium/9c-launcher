a14a50dd522540b90cb1865eec83b13a4d76d115
Atralupus <me@atralupus.com>
2022-08-19 15:29:07 +0900

    feat: Cleanup old player in init player

--- src/main/update/util.ts
+++ src/main/update/util.ts
@@ -1,6 +1,9 @@
 import { NotSupportedPlatformError } from "../exceptions/not-supported-platform";
 import { decode, BencodexDict } from "bencodex";
 import { DEFAULT_DOWNLOAD_BASE_URL, get as getConfig } from "../../config";
+import path from "path";
+import { app } from "electron";
+import fs from "fs";
 
 export function decodeLocalAPV(): BencodexDict | undefined {
   const localApvToken = getConfig("AppProtocolVersion");
@@ -41,3 +44,20 @@ const FILENAME_MAP: { [k in NodeJS.Platform]: string | null } = {
   cygwin: "win.zip",
   netbsd: null,
 };
+
+export function cleanupOldPlayer() {
+  const OLD_MAC_GAME_PATH = "9c.app/Contents/MacOS/9c";
+  const OLD_WIN_GAME_PATH = "9c.exe";
+  const OLD_LINUX_GAME_PATH = "9c";
+
+  const oldPlayerPath = path.join(
+    app.getAppPath(),
+    process.platform === "darwin"
+      ? OLD_MAC_GAME_PATH
+      : process.platform === "linux"
+      ? OLD_LINUX_GAME_PATH
+      : OLD_WIN_GAME_PATH
+  );
+
+  fs.unlinkSync(oldPlayerPath);
+}
