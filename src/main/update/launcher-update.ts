df8b17bc8f87a8fd871dfcd5f0cb0309b9ef6829
Atralupus <me@atralupus.com>
2022-08-23 11:30:13 +0900

    feat: Support linux

--- src/main/update/launcher-update.ts
+++ src/main/update/launcher-update.ts
@@ -250,7 +250,7 @@ export async function update(update: Update, listeners: IUpdateOptions) {
       console.warn("Failed to remove temporary files from", tempDir, "\n", e);
     }
     win.webContents.send("update copying complete");
-  } else if (process.platform == "darwin") {
+  } else if (process.platform == "darwin" || process.platform == "linux") {
     // untar .tar.{gz,bz2}
     const lowerFname = dlFname.toLowerCase();
     const bz2 = lowerFname.endsWith(".tar.bz2") || lowerFname.endsWith(".tbz");
--- src/main/update/player-update-temp.ts
+++ src/main/update/player-update-temp.ts
@@ -116,7 +116,7 @@ export async function playerUpdateTemp(
       },
     });
     win.webContents.send("update extract complete");
-  } else if (process.platform == "darwin") {
+  } else if (process.platform == "darwin" || process.platform == "linux") {
     // untar .tar.{gz,bz2}
     const lowerFname = dlFname.toLowerCase();
     const bz2 = lowerFname.endsWith(".tar.bz2") || lowerFname.endsWith(".tbz");
