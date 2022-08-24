3b729c2b31afdded205c09a00442b59e50a7429c
Basix <basix@basix.tech>
2022-08-24 19:06:55 +0900

    Enable Fast Refresh on Storybook
    https://storybook.js.org/docs/react/faq#how-do-i-setup-react-fast-refresh-with-storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,34 +1,36 @@
-const rendererConfig = require('../webpack.config.js');
-const path = require('path');
+const rendererConfig = require("../webpack.config.js");
+const path = require("path");
 
 module.exports = {
-  "core": {
-    "builder": "webpack5",
+  core: {
+    builder: "webpack5",
   },
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-actions",
     "@storybook/addon-essentials",
     "@storybook/preset-scss",
     "storybook-addon-apollo-client",
   ],
-  webpackFinal: async (config, {configType}) => {
+  reactOptions: {
+    fastRefresh: true,
+  },
+  webpackFinal: async (config, { configType }) => {
     const custom = rendererConfig({
       NODE_ENV: process.env.NODE_ENV || "development",
       target: "renderer",
       release: false,
     });
 
-    config.resolve.modules = [
-      path.resolve(__dirname, ".."),
-      "node_modules",
-    ]
+    config.resolve.modules = [path.resolve(__dirname, ".."), "node_modules"];
     config.resolve.alias.electron = require.resolve("../__tests__/ipcMock.ts");
-    config.resolve.alias['tmp-promise'] = require.resolve("../__tests__/tmpMock.ts");
-    return { ...config, module: { ...config.module, rules: custom.module.rules } };
-  }
-}
+    config.resolve.alias["tmp-promise"] = require.resolve(
+      "../__tests__/tmpMock.ts"
+    );
+    return {
+      ...config,
+      module: { ...config.module, rules: custom.module.rules },
+    };
+  },
+};
