04299a6750008faf767f980af0c9a4172c664e10
Basix <basix@basix.tech>
2022-08-29 16:09:10 +0900

    Configure Vitest

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      src: "./src",
+    },
+  },
+  rollupOptions: {
+    external: ["electron", "spawn-sync"],
+    output: {
+      globals: {
+        "spawn-sync": "require('child_process').spawnSync",
+      },
+    },
+  },
+  test: {},
+});
