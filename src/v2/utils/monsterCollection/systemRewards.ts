9288d335b7662ce1119640e8b1d2300f359d1086
Basix <basix@basix.tech>
2022-08-30 15:03:54 +0900

    Update collection stats

--- src/v2/utils/monsterCollection/systemRewards.ts
+++ src/v2/utils/monsterCollection/systemRewards.ts
@@ -2,26 +2,26 @@ export default [
   {
     crystal: 0,
     arena: 0,
-    stage: 6,
+    stage: 0,
   },
   {
     crystal: 50,
     arena: 100,
-    stage: 5,
+    stage: 0,
   },
   {
     crystal: 100,
     arena: 200,
-    stage: 5,
+    stage: 20,
   },
   {
     crystal: 200,
     arena: 200,
-    stage: 4,
+    stage: 20,
   },
   {
     crystal: 300,
     arena: 200,
-    stage: 4,
+    stage: 40,
   },
 ];
