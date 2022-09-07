1f54492a3bcd19f4ead2363b65b25e4f88d80e96
Basix <basix@basix.tech>
2022-09-07 15:34:58 +0900

    Use WHATWG URL for stroing url

--- src/v2/utils/useExternalURL.tsx
+++ src/v2/utils/useExternalURL.tsx
@@ -5,17 +5,19 @@ import { IPC_OPEN_URL } from "../ipcTokens";
 let urlBeforeReact: string | null = null;
 once(IPC_OPEN_URL, (_, url) => (urlBeforeReact = url));
 
-const context = createContext<string | null>(null);
+const context = createContext<URL | null>(null);
 
 const { Provider } = context;
 
 export function ExternalURLProvider({ children }: React.PropsWithChildren<{}>) {
-  const [url, setUrl] = useState<string | null>(urlBeforeReact);
+  const [url, setUrl] = useState<URL | null>(
+    urlBeforeReact !== null ? new URL(urlBeforeReact) : null
+  );
 
   useEffect(
     () =>
       on(IPC_OPEN_URL, (_, url) => {
-        setUrl(url);
+        setUrl(new URL(url));
       }),
     []
   );
@@ -23,6 +25,6 @@ export function ExternalURLProvider({ children }: React.PropsWithChildren<{}>) {
   return <Provider value={url}>{children}</Provider>;
 }
 
-export function useExternalURL(): string | null {
+export function useExternalURL(): URL | null {
   return useContext(context);
 }
