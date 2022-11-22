a15ac28a3051b5cbe1b56010a7e75a22e9196055
Yang Chun Ung <qooraven@gmail.com>
2022-11-22 21:20:01 +0900

    Add rune reward

--- src/v2/utils/monsterCollection/items.ts
+++ src/v2/utils/monsterCollection/items.ts
@@ -1,5 +1,6 @@
 import hourglassImg from "src/v2/resources/collection/items/400000.png";
 import APpotionImg from "src/v2/resources/collection/items/500000.png";
+import RuneImg from "src/v2/resources/collection/items/20001.png";
 
 export default {
   400000: {
@@ -9,5 +10,9 @@ export default {
   500000: {
     name: "AP Potion",
     img: APpotionImg,
+  },
+  20001: {
+    name: "Rune Stone",
+    img: RuneImg,
   },
 } as Record<number, { name: string; img: string }>;
