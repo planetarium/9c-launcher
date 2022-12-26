53e8fcee2b6f026931b218f7312c46b6c6dee2a7
Akamig <akamig@posteo.net>
2022-12-27 00:42:44 +0900

    Remove Headless Related Scripts from CI

--- scripts/build-headless.ps1
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env pwsh
-dotnet publish NineChronicles.Headless/NineChronicles.Headless.Executable/NineChronicles.Headless.Executable.csproj `
-  -c Release `
-  -r win-x64 `
-  -o dist/publish `
-  --self-contained `
-  --version-suffix "$(git -C NineChronicles.Headless rev-parse HEAD)"
