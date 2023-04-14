892c468df3ab195cbcc3d5ba7e4bf9bbf323c90b
dabin0127 <duekthtjf783@gmail.com>
2023-04-14 20:00:33 +0900

    feat: Separate the disk space search function.

--- src/utils/file.ts
+++ src/utils/file.ts
@@ -2,16 +2,26 @@ import { exec } from "child_process";
 
 type OSPlatform = "darwin" | "linux" | "win32";
 
+function getCommandOfAvailableDisk(path: string): string {
+  const platform = process.platform as OSPlatform;
+
+  let cmd: string = "";
+
+  if (platform === "darwin" || platform === "linux") {
+    cmd = `df -k ${path} | tail -1 | awk '{print $4}'`;
+  } else if (platform === "win32") {
+    cmd = `for /f "skip=1" %p in ('wmic logicaldisk get FreeSpace^,Size^,Caption /format:list ^| find /i "${path}"') do @echo %p`;
+  }
+
+  return cmd;
+}
+
 export function getAvailableDiskSpace(path: string = "/"): Promise<number> {
   return new Promise((resolve, reject) => {
-    const platform = process.platform as OSPlatform;
-    let cmd: string;
-    if (platform === "darwin" || platform === "linux") {
-      cmd = `df -k ${path} | tail -1 | awk '{print $4}'`;
-    } else if (platform === "win32") {
-      cmd = `for /f "skip=1" %p in ('wmic logicaldisk get FreeSpace^,Size^,Caption /format:list ^| find /i "${path}"') do @echo %p`;
-    } else {
-      return reject(new Error(`Unsupported platform: ${platform}`));
+    const cmd = getCommandOfAvailableDisk(path);
+
+    if (!cmd) {
+      return reject(new Error(`Unsupported platform: ${process.platform}`));
     }
 
     exec(cmd, (err, stdout, stderr) => {
