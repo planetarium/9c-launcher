7b141288276d14f52c1b3ad5f42b9dd6ba759dde
Akamig <akamig@posteo.net>
2023-06-19 18:41:50 +0900

    Change Key Renderer
    (cherry picked from commit dd7326ff5bc6d7cf2c0fc7d3fce7fade5583bc1a)

--- src/renderer/components/core/Layout/UserInfo/ExportOverlay.tsx
+++ src/renderer/components/core/Layout/UserInfo/ExportOverlay.tsx
@@ -1,17 +1,21 @@
-import { styled } from "@material-ui/core";
-import React, { useMemo } from "react";
-import bwipjs from "bwip-js";
+import { styled, Box } from "@material-ui/core";
+import React, { useEffect, useState } from "react";
+import bitgener from "bitgener";
+import DOMPurify from "dompurify";
 import { useStore } from "src/utils/useStore";
 import OverlayBase, { CloseButton } from "../../OverlayBase";
+import Text from "src/renderer/components/ui/Text";
+import H1 from "src/renderer/components/ui/H1";
 import { OverlayProps } from "src/utils/types";
 
 const MainPageContainer = styled(OverlayBase)({
   "&&": {
-    width: "650px",
-    height: "650px",
+    width: "420px",
+    height: "600px",
     backgroundColor: "#1d1e1f",
     padding: "48px 48px",
     display: "flex",
+    gap: "1rem",
     flexDirection: "column",
     alignItems: "center",
     color: "white",
@@ -19,30 +23,43 @@ const MainPageContainer = styled(OverlayBase)({
 });
 
 export function ExportOverlay({ isOpen, onClose }: OverlayProps) {
+  const [vector, setVector] = useState<{ __html: string }>();
   const account = useStore("account");
-  const renderDMX = async () => {
-    const keystoreString = await account.exportKeystore();
-    if (keystoreString) {
-      try {
-        return bwipjs.toCanvas("keystoreDMX", {
-          bcid: "datamatrix", // Barcode type
-          text: keystoreString, // Text to encode
-          scale: 3, // 3x scaling factor
+  useEffect(() => {
+    (async () => {
+      const key = await account.exportKeystore();
+      if (key && key !== "") {
+        const matrix = await bitgener({
+          data: key,
+          type: "datamatrix",
+          output: "string",
+          width: 360,
+          height: 360,
+          hri: {
+            show: false,
+            marginTop: 0,
+          },
         });
-      } catch (e) {
-        console.error(e);
+        if (matrix.svg) {
+          setVector({
+            __html: DOMPurify.sanitize(matrix.svg, {
+              USE_PROFILES: { svg: true, svgFilters: true },
+            }),
+          });
+        }
       }
-    }
-  };
-  useMemo(renderDMX, [renderDMX]);
+    })();
+  }, []);
 
   return (
     <MainPageContainer isOpen={isOpen} onDismiss={onClose}>
       <CloseButton onClick={() => onClose()} />
-      <canvas
-        style={{ backgroundColor: "white", padding: "1rem", width: "100%" }}
-        id="keystoreDMX"
-      ></canvas>
+      <H1 css={{ margin: 0 }}>Key Export</H1>
+      <div dangerouslySetInnerHTML={vector} />
+      <Box>
+        <Text>This is Encrypted Key.</Text>
+        <Text>Do Not Show Your Key to Others.</Text>
+      </Box>
     </MainPageContainer>
   );
 }
