5c2cda0dd7c68c6385cc74be2b52c92b54072f1c
Atralupus <me@atralupus.com>
2023-06-27 00:33:20 +0900

    fix: Add defense update condition

--- src/main/update/updater.ts
+++ src/main/update/updater.ts
@@ -2,6 +2,7 @@ import { UpdateInfo, autoUpdater } from "electron-updater";
 import log from "electron-log";
 import { netenv } from "src/config";
 import { IUpdateOptions } from "./types";
+import { app } from "electron";
 
 class AppUpdater {
   win: Electron.BrowserWindow;
@@ -39,18 +40,38 @@ class AppUpdater {
   }
 
   handleUpdateAvailable(updateInfo: UpdateInfo) {
-    console.log("Found update", updateInfo);
+    console.log(
+      `Found update (local version: ${app.getVersion()})`,
+      updateInfo
+    );
   }
 
   handleUpdateDownloaded(updateInfo: UpdateInfo) {
-    this.win.webContents.executeJavaScript(
-      `window.location.hash = '/confirm-update'`
-    );
+    if (isVersionGreaterThan(updateInfo.version, app.getVersion())) {
+      this.win.webContents.executeJavaScript(
+        `window.location.hash = '/confirm-update'`
+      );
+    }
   }
 
   handleError(error: Error) {
     console.info("Updater error occurred", error);
   }
+}
+
+function isVersionGreaterThan(v1: string, v2: string) {
+  const v1Parts = v1.split(".").map(Number);
+  const v2Parts = v2.split(".").map(Number);
+
+  for (let i = 0; i < v1Parts.length; ++i) {
+    if (v1Parts[i] > v2Parts[i]) {
+      return true;
+    } else if (v1Parts[i] < v2Parts[i]) {
+      return false;
+    }
+  }
+
+  return false;
 }
 
 export default AppUpdater;
