88ebf417fe1fa88e85a9bda403836e570d824f50
Atralupus <me@atralupus.com>
2023-06-27 13:39:05 +0900

    ci: Separate worker

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,5 +1,8 @@
 network=$1
+targets=${2:-"mwl"}
+
+echo "$network, $targets"
 
 yarn
 yarn run build-$network
-yarn run electron-builder -mwl --arm64 --x64 --publish always --config ./electron-builder.$network.yml
+yarn run electron-builder -$targets --arm64 --x64 --publish always --config ./electron-builder.$network.yml
