9a55c5a6ff446694acddebcf8184d2eb4938c18f
Akamig <akamig@posteo.net>
2023-10-16 14:37:54 +0900

    Store Name Change

--- src/stores/planetary.ts
+++ src/stores/planetary.ts
@@ -1,7 +1,8 @@
 import { action, makeObservable, observable } from "mobx";
 import { NodeInfo } from "src/config";
+import { Planet } from "src/interfaces/registry";
 
-export default class RpcStore {
+export default class PlanetaryStore {
   public constructor() {
     makeObservable(this);
   }
