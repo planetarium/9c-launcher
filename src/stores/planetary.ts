1310546638953efe71a5c9bd6239dd7ef5e61392
Akamig <akamig@posteo.net>
2023-10-19 02:46:27 +0900

    Overhaul planetStore

--- src/stores/planetary.ts
+++ src/stores/planetary.ts
@@ -5,64 +5,78 @@ import { initializeNode } from "src/config";
 import { ipcRenderer } from "electron";
 
 export default class PlanetaryStore {
-  @observable
-  public node!: NodeInfo;
+  public constructor() {}
 
-  @observable
-  public host: string = "";
-
-  @observable
-  public rpcPort: number = 31238;
-
-  @observable
-  public registry: Planet[] = [];
-
-  @observable
-  public planet!: Planet;
-
-  @action
-  public async setPlanet(planetID: string) {
-    const planet = this.getPlanetById(planetID);
-    if (planet === undefined) {
-      throw Error("No matching planet ID found");
-    }
-    this.planet = planet;
-    this.updateConfigToRegistry();
-    await this.setNodeFromPlanet(planet);
-  }
-
-  public constructor() {
-    fetch(configStore.get("PlanetRegistryUrl"), {}).then(async (data) => {
-      this.registry = await data.json();
-      await this.setPlanet(get("Planet", "0x000000000000"));
-    });
+  public init(registry: Planet[], node: NodeInfo) {
+    this.registry = registry;
+    this.setPlanet(get("Planet", "0x000000000000"));
+    this.setNode(node);
     makeObservable(this);
   }
 
-  @action
-  public getPlanetById = (id: string): Planet | undefined => {
-    return this.registry
-      ? this.registry.find((planet) => planet.id === id)
-      : undefined;
-  };
+  @observable
+  node: NodeInfo | null = null;
+  @observable
+  registry!: Planet[];
+  @observable
+  planet!: Planet;
 
-  private async setNodeFromPlanet(planet: Planet) {
+  @action
+  public setRegistry(registry: Planet[]) {
+    this.registry = registry;
+  }
+
+  @action
+  public getHost() {
+    return new URL(this.node!.gqlUrl).host;
+  }
+
+  @action
+  public getRpcPort() {
+    return Number.parseInt(new URL(this.node!.grpcUrl).port);
+  }
+
+  @action
+  public setPlanet(planetID: string) {
+    const planet = this.getPlanetById(planetID);
+    if (planet === undefined) {
+      console.error("No matching planet ID found, Using Default.");
+      this.planet = this.registry[0];
+    } else this.planet = planet;
+    this.updateConfigToPlanet();
+  }
+
+  @action
+  public async changePlanet(id: string) {
+    this.setPlanet(id);
+    await this.setNodeFromPlanet();
+    this.updateConfigToPlanet();
+  }
+
+  @action.bound
+  public getPlanetById(id: string): Planet | undefined {
+    return this.registry.find((planet) => planet.id === id);
+  }
+
+  private setNode(node: NodeInfo) {
+    this.node = node;
+  }
+
+  private async setNodeFromPlanet() {
     try {
-      this.node = await initializeNode(planet.rpcEndpoints);
-      this.host = new URL(this.node.gqlUrl).host;
-      this.rpcPort = Number.parseInt(new URL(this.node.grpcUrl).port);
+      this.setNode(await initializeNode(this.planet.rpcEndpoints));
     } catch (e) {
-      console.error(e);
-      ipcRenderer.invoke("all-rpc-failed").then((v: boolean) => {
-        this.setNodeFromPlanet(this.planet);
+      console.error("Failed to set node from planet:", e);
+      ipcRenderer.invoke("all-rpc-failed").then(async () => {
+        await this.setNodeFromPlanet();
       });
     }
   }
 
-  private updateConfigToRegistry = () => {
+  private updateConfigToPlanet() {
     if (this.planet) {
       configStore.set("GenesisBlockPath", this.planet.genesisUri);
       configStore.set("DataProviderUrl", this.planet.rpcEndpoints["dp.gql"]);
     }
-  };
+  }
 }
