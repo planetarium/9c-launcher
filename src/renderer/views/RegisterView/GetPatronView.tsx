29827abaf3074b30659e977fe7318fa6913cab63
Akamig <akamig@posteo.net>
2023-10-30 13:18:51 +0900

    Added Labels

--- src/renderer/views/RegisterView/GetPatronView.tsx
+++ src/renderer/views/RegisterView/GetPatronView.tsx
@@ -9,7 +9,12 @@ import { useStore } from "src/utils/useStore";
 import { useCheckContractedQuery } from "src/generated/graphql";
 import { registerStyles } from ".";
 import TextField from "src/renderer/components/ui/TextField";
-import { Select, SelectOption } from "src/renderer/components/ui/Select";
+import {
+  Select,
+  SelectLabel,
+  SelectOption,
+  SelectWrapper,
+} from "src/renderer/components/ui/Select";
 import { ExtLink } from "src/renderer/components/ui/Link";
 import Button from "src/renderer/components/ui/Button";
 import { useHistory } from "react-router";
@@ -19,6 +24,7 @@ const transifexTags = "v2/views/register/GetPatronView";
 function GetPatronView() {
   const [disable, setDisable] = useState(false);
   const [planetId, setPlanetId] = useState<string>(get("Planet"));
+  const [switching, setSwitching] = useState(false);
   const { account, planetary } = useStore();
   const { data, refetch } = useCheckContractedQuery({
     variables: {
@@ -28,9 +34,11 @@ function GetPatronView() {
   const history = useHistory();
 
   const switchPlanet = (id: string) => {
+    setSwitching(true);
     setPlanetId(id);
-    planetary.changePlanet(id).then((v) => {
+    planetary.changePlanet(id).then(() => {
       refetch();
+      setSwitching(false);
     });
   };
 
@@ -52,28 +60,32 @@ function GetPatronView() {
           _tags={transifexTags}
         />
       </p>
+      <SelectWrapper>
+        <SelectLabel id="planet-label">Planet</SelectLabel>
+        <Select
+          labelId="planet-label"
+          value={planetId}
+          defaultValue={planetary.planet.id}
+          onChange={(event) => switchPlanet(event.target.value as string)}
+          label="Planet"
+        >
+          {planetary.registry.map((entry) => (
+            <SelectOption key={entry.id} value={entry.id}>
+              {entry.name}
+            </SelectOption>
+          ))}
+        </Select>
+      </SelectWrapper>
       <TextField
         label="Address"
         readOnly
         value={account.loginSession?.address.toString()}
       />
-
-      <Select
-        value={planetId}
-        defaultValue={planetary.planet.id}
-        onChange={(event) => switchPlanet(event.target.value as string)}
-      >
-        {planetary.registry.map((entry) => (
-          <SelectOption key={entry.id} value={entry.id}>
-            {entry.name}
-          </SelectOption>
-        ))}
-      </Select>
       <Button
         variant="primary"
         centered
         css={{ fontSize: 24, width: 360, marginTop: 25, fontWeight: 600 }}
-        disabled={disable}
+        disabled={disable || switching}
         onClick={() => {
           shell.openExternal(
             get("ActivationCodeUrl") +
