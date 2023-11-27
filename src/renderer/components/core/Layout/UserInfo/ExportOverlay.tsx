2e4c38ad33f58f7ced173a350f01459fba522c25
Akamig <akamig@posteo.net>
2023-11-27 19:41:10 +0900

    Add DummyQR to hide real QR

--- src/renderer/components/core/Layout/UserInfo/ExportOverlay.tsx
+++ src/renderer/components/core/Layout/UserInfo/ExportOverlay.tsx
@@ -1,5 +1,6 @@
 import { styled, Box, Button, Backdrop } from "@material-ui/core";
 import React, { useEffect, useState } from "react";
+import DummyQR from "src/renderer/resources/dummyQR.png";
 import writeQR from "@paulmillr/qr";
 import { useStore } from "src/utils/useStore";
 import OverlayBase, { CloseButton } from "../../OverlayBase";
@@ -12,9 +13,9 @@ const MainPageContainer = styled(OverlayBase)({
     width: "420px",
     height: "600px",
     backgroundColor: "#1d1e1f",
-    padding: "48px 48px",
+    padding: "2rem",
     display: "flex",
-    gap: "1rem",
+    gap: "0.5rem",
     flexDirection: "column",
     alignItems: "center",
     color: "white",
@@ -25,22 +26,20 @@ const SquareBox = {
   borderRadius: "1rem",
   aspectRatio: "1/1",
   minWidth: "100%",
-  flex: "1 1 0",
-};
 
-const hiddenBox = {
-  ...SquareBox,
   display: "flex",
-  alignItems: "center",
   justifyContent: "center",
-  fontSize: "3rem",
-  background: "grey",
+  overflow: "hidden",
 };
 
 const imageBox = {
-  ...SquareBox,
   backgroundColor: "white",
   padding: "1rem",
+};
+
+const hiddenBox = {
+  ...imageBox,
+  filter: "blur(0.5rem) brightness(1.2)",
 };
 
 export function ExportOverlay({ isOpen, onClose }: OverlayProps) {
@@ -63,11 +62,13 @@ export function ExportOverlay({ isOpen, onClose }: OverlayProps) {
         }}
       />
       <H1 css={{ margin: 0 }}>Key Export</H1>
-      {hidden ? (
-        <Box style={hiddenBox}>HIDDEN</Box>
-      ) : (
-        <img style={imageBox} src={`data:image/svg+xml;utf8,${vector}`} />
-      )}
+      <Box style={SquareBox}>
+        {hidden ? (
+          <img style={hiddenBox} src={DummyQR}></img>
+        ) : (
+          <img style={imageBox} src={`data:image/svg+xml;utf8,${vector}`} />
+        )}
+      </Box>
       <Box marginBottom={0}>
         <Text>This is Encrypted Key.</Text>
         <Text>Do Not Show Your Key to Others.</Text>
