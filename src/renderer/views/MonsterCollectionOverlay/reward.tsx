9249643e8d9d276f84c444155e30ccf2df704a87
Akamig <akamig@posteo.net>
2024-01-11 09:59:12 +0900

    fix: Use KMB

--- src/renderer/views/MonsterCollectionOverlay/reward.tsx
+++ src/renderer/views/MonsterCollectionOverlay/reward.tsx
@@ -174,7 +174,7 @@ const ItemTitle = styled("h2", {
 });
 
 const ItemAmount = styled("div", {
-  fontSize: "12px",
+  fontSize: "14px",
   color: "#fff5e3",
   textShadow: theme.shadows.standard,
   WebKitTextStroke: "1px solid black",
@@ -218,12 +218,12 @@ const ItemBox = styled("div", {
 interface ItemProps {
   children: React.ReactNode;
   title: React.ReactNode;
-  amount?: string | number;
+  amount: string | number;
   received?: boolean;
   tooltip?: React.ReactNode;
   isUpgrade?: boolean;
   isDiff?: boolean;
-  updatedAmount?: string | number;
+  updatedAmount: string | number;
 }
 
 export const Item = ({
@@ -233,19 +233,28 @@ export const Item = ({
   updatedAmount,
   isUpgrade,
   isDiff,
-}: ItemProps) => (
-  <ItemBox>
-    <ItemFrame>
-      {children}
-      <ItemAmount>
-        {amount}
-        {isUpgrade != null && isDiff && (
-          <ItemUpdatedAmount isUpgrade={isUpgrade} isDiff={isDiff ?? false}>
-            {updatedAmount}
-          </ItemUpdatedAmount>
-        )}
-      </ItemAmount>
-    </ItemFrame>
-    <ItemTitle>{title}</ItemTitle>
-  </ItemBox>
-);
+}: ItemProps) => {
+  const numFormat = new Intl.NumberFormat("en-US", {
+    notation: "compact",
+    maximumFractionDigits: 2,
+  });
+
+  return (
+    <ItemBox>
+      <ItemFrame>
+        {children}
+        <ItemAmount>
+          {Number.isNaN(+amount) ? amount : numFormat.format(+amount)}
+          {isUpgrade != null && isDiff && (
+            <ItemUpdatedAmount isUpgrade={isUpgrade} isDiff={isDiff ?? false}>
+              {Number.isNaN(+updatedAmount)
+                ? updatedAmount
+                : numFormat.format(+updatedAmount)}
+            </ItemUpdatedAmount>
+          )}
+        </ItemAmount>
+      </ItemFrame>
+      <ItemTitle>{title}</ItemTitle>
+    </ItemBox>
+  );
+};
