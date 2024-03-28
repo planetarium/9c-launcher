db008153117cc639caea8499b09ea40a6617dbbe
sky1045 <ghdwlgud4545@gmail.com>
2024-03-28 18:14:44 +0900

    Fix getting timestamp

--- scripts/purge-ncloud-cdn.sh
+++ scripts/purge-ncloud-cdn.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-OS=$1
-ACCESS_KEY=$2
-SECRET_KEY=$3
+ACCESS_KEY=$1
+SECRET_KEY=$2
 API_GATEWAY_URL="https://ncloud.apigw.ntruss.com"
-RESOURCE_URL="/cdn/v2/requestGlobalCdnPurge?cdnInstanceNo=$4&isWholePurge=true&isWholeDomain=true&responseFormatType=JSON"
-if [[ $OS = "macOS" ]]; then
-  TIMESTAMP=$(echo $(($(gdate +%s%N)/1000000)))
-else
-  TIMESTAMP=$(echo $(($(date +%s%N)/1000000)))
-fi
+RESOURCE_URL="/cdn/v2/requestGlobalCdnPurge?cdnInstanceNo=$3&isWholePurge=true&isWholeDomain=true&responseFormatType=JSON"
+TIMESTAMP=$(python -c 'import time; print(int(time.time() * 1000))')
 
 function makeSignature() {
     nl=$'\\n'
