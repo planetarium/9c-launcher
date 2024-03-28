77b6233f156ff1f2e7a5ffaedba94178617ed27e
sky1045 <ghdwlgud4545@gmail.com>
2024-03-28 15:32:00 +0900

    Add NCloud CDN Purge after release

---
+++ scripts/purge-ncloud-cdn.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+ACCESS_KEY=$1
+SECRET_KEY=$2
+API_GATEWAY_URL="https://ncloud.apigw.ntruss.com"
+RESOURCE_URL="/cdn/v2/requestGlobalCdnPurge?cdnInstanceNo=$3&isWholePurge=true&isWholeDomain=true&responseFormatType=JSON"
+TIMESTAMP=$(echo $(($(date +%s%N)/1000000)))
+
+function makeSignature() {
+    nl=$'\\n'
+
+    ACCESSKEY=$ACCESS_KEY
+    SECRETKEY=$SECRET_KEY
+
+    METHOD="GET"
+    URI=$RESOURCE_URL
+
+    SIG="$METHOD"' '"$URI"${nl}
+    SIG+="$TIMESTAMP"${nl}
+    SIG+="$ACCESSKEY"
+
+    SIGNATURE=$(echo -n -e "$SIG"|iconv -t utf8 |openssl dgst -sha256 -hmac $SECRETKEY -binary|openssl enc -base64)
+    echo $SIGNATURE
+}
+
+SIGNATURE=$(makeSignature)
+
+curl -vs "$API_GATEWAY_URL$RESOURCE_URL" \
+    -H "Content-Type: application/json" \
+    -H "x-ncp-apigw-timestamp:$TIMESTAMP" \
+    -H "x-ncp-iam-access-key:$ACCESS_KEY" \
+    -H "x-ncp-apigw-signature-v2:$SIGNATURE"
