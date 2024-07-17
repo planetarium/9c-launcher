3f7f9c299e61b1bb62ad001c6d18c0c731db0757
Akamig <akamig@posteo.net>
2024-07-17 13:56:34 +0900

    Fix: Revive FileChooser, pass file instead of text.

--- src/renderer/components/ImportInput.tsx
+++ src/renderer/components/ImportInput.tsx
@@ -2,17 +2,21 @@ import { t } from "@transifex/native";
 import React from "react";
 // import FileChooser from "./ui/FileChooser";
 import TextField from "./ui/TextField";
+import FileChooser from "./ui/FileChooser";
 
 export interface ImportData {
   key?: string;
+  keyFile?: File;
   fromFile?: boolean;
 }
 
-type Action = React.ChangeEvent<HTMLInputElement> | string;
+type Action = React.ChangeEvent<HTMLInputElement> | string | File;
 
 function make(action: Action): ImportData {
   if (typeof action === "string") {
     return { key: action, fromFile: true };
+  } else if (action instanceof File) {
+    return { keyFile: action, fromFile: true };
   } else {
     if (action.target.value.length === 0) return {};
     return { key: action.target.value, fromFile: false };
@@ -29,10 +33,12 @@ export interface ImportInputProps {
 export default function ImportInput({ onSubmit, fromFile }: ImportInputProps) {
   return (
     <>
-      {/* <FileChooser
-        disabled={!fromFile && fromFile != null}
-        onDrop={(files) => files[0]?.text()?.then((v) => onSubmit(make(v)))}
-      /> */}
+      {
+        <FileChooser
+          disabled={!fromFile && fromFile != null}
+          onDrop={(files) => onSubmit(make(files[0]))}
+        />
+      }
       <TextField
         disabled={!!fromFile}
         onChange={(v) => onSubmit(make(v))}
