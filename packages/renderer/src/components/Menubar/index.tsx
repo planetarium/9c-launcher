09c6bf6ef9601ff5995b3ce559e1c8d1b3efd9ab
Akamig <akamig@posteo.net>
2024-09-12 12:20:41 +0900

    Base Components / Layout

---
+++ packages/renderer/src/components/Menubar/index.tsx
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled';
+
+export const Menu = styled.div({
+  color: 'white',
+  background: 'rgba(31, 32, 35, 0.70)',
+  border: '2px solid transparent',
+  borderImage: 'linear-gradient(#7A7369, #EAC48B ,#7A7369)',
+  borderImageSlice: '1',
+  backdropFilter: 'blur(4px)',
+  display: 'flex',
+  padding: '4px 16px',
+  alignItems: 'flex-start',
+  gap: '1rem',
+});
