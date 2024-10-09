5f385467d2c0bf9421e1fcbcd4ff0af1fe1162bc
Akamig <akamig@posteo.net>
2024-10-10 05:27:56 +0900

    Add Libraries

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./packages/renderer/index.html",
+    "./packages/renderer/src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
