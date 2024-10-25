c813cbe7b7ad5a8621f09348e5b7736350b77e13
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-10-25 22:30:26 +0900

    refactor: ImportView 컴포넌트 단순화
    - useKeyImport 훅 적용으로 상태 관리 로직 분리

--- src/renderer/views/ImportView/index.tsx
+++ src/renderer/views/ImportView/index.tsx
@@ -36,14 +36,7 @@ function ImportView() {
         <Button onClick={history.goBack.bind(history)}>
           <T _str="Prev" _tags={transifexTags} />
         </Button>
-        <Button
-          variant="primary"
-          disabled={
-            (!key.key && !key.keyFile) ||
-            (!!key.key && !account.isValidPrivateKey(key.key))
-          }
-          onClick={handleSubmit}
-        >
+        <Button variant="primary" disabled={!isKeyValid} onClick={handleSubmit}>
           <T _str="Next" _tags={transifexTags} />
         </Button>
       </ButtonBar>
