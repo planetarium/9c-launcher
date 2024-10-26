dc655e8722bc688a6d4c7136f0d4a08654947637
gongjuyeonhee <yyeonhee912@naver.com>
2024-10-26 13:24:12 +0900

    fix: resolve confilct

--- src/renderer/hooks/useKeyImport.ts
+++ src/renderer/hooks/useKeyImport.ts
@@ -13,11 +13,48 @@ export default function useKeyImport() {
   const [key, setKey] = useState<ImportData>({});
   const [error, setError] = useState<string | null>(null);
 
+  // 파일 이름 정규식으로 검증
+  const isValidFileName = (fileName: string) => {
+    const regex = /^[a-z0-9]{3,15}\.(txt|json)$/;
+    return regex.test(fileName);
+  };
+
+  //파일 형식 검증
+  const isImageFile = (fileName: string) => {
+    const imageExtensions = /\.(png|jpg|jpeg|gif)$/i;
+    return imageExtensions.test(fileName);
+  };
+
   const handleSubmit = async () => {
     let privateKey: RawPrivateKey;
     if (key.keyFile) {
+      // QR 디코딩을 사용하지 않고 파일을 그대로
+      const fileName = key.keyFile.name;
+
+      //
+      if (!isValidFileName(fileName)) {
+        setError(t("Invalid file name format."));
+        return;
+      }
+
       try {
-        const keystore = await decodeQRCode(key.keyFile);
+        //qr디코딩 없이 일반 파일로 처리
+        const keystore = await key.keyFile.text();
+
+        //이미지 파일일 경우 qr코드 디코드
+        if (isImageFile(fileName)) {
+          const keystore = await decodeQRCode(key.keyFile);
+        }
+
+        // JSON 검증
+        try {
+          JSON.parse(keystore);
+        } catch (e) {
+          setError(t("Invalid JSON format in the file."));
+          return;
+        }
+
+        // const keystore = await decodeQRCode(key.keyFile);
         const { id, address }: { id: string; address: string } =
           JSON.parse(keystore);
         try {
